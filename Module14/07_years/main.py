a87818e3c70b590afa5d5ba28439be55689a077d
Ilnur <ilnrzakirov@gmail.com>
2021-01-16 19:14:07 +0300

    Задача 7 решена

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,1 +1,28 @@
-# TODO здесь писать код
+first_year = int(input("Введите первый год: "))
+second_year = int(input("Введите второй год: "))
+
+
+print(f"Года от {first_year} до {second_year} с тремя одинаковыми цифрами: ")
+
+
+for year in range (first_year, second_year + 1):
+    remains = ""
+    i = 1
+    i_1 = 1
+    remains_1 = ""
+    for symbol in str(year):
+        if remains == symbol:
+            i += 1
+            continue
+        elif remains_1 == symbol:
+            i_1 += 1
+            continue
+        elif remains == "":
+            remains = symbol
+        else:
+            remains_1 = symbol
+
+    if i >= 3 or i_1 >= 3:
+        print(year)
+
+
