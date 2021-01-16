c97370aac199b7ba46630de1cba1f0d4e932adb6
Insolente13 <lanc05@mail.ru>
2021-01-16 17:21:51 +0300

    Module 14 02 - okб 03 - TODO

--- Module14/03_sum_and_dif/main.py
+++ Module14/03_sum_and_dif/main.py
@@ -1,21 +1,25 @@
 n = int(input("Введите число : "))
 
-def summ (number):
+# TODO, пожалуйста поправьте нейминг. Пока что не очень ясно, что значит переменная "i".
+def summ(number):
     i = 0
-    while number !=0:
+    while number != 0:
         remains = number % 10
         i += remains
         number //= 10
     print(f"Сумма чисел: {i}")
     return i
 
-def quantity (number):
+
+def quantity(number):
     i = 0
     while number != 0:
         i += 1
         number //= 10
     print(f"Количесвто цифр в числе: {i}")
     return i
+
+
 summ = summ(n)
 quantity = quantity(n)
 print(f"Разность суммы и количества цифр : {summ - quantity}")
