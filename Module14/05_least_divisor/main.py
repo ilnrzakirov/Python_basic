c1da7ef2061c9b0dfc892856ee34a208cae200af
Insolente13 <lanc05@mail.ru>
2021-01-16 18:28:06 +0300

    Module 14 03, 05, 06 - ok

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,16 +1,18 @@
 n = int(input("Введите число: "))
 
-def divider (n):
+
+def divider(n):
     result = 0
-    for number in range (2, n + 1):
+    for number in range(2, n + 1):
         if n % number == 0:
             result = number
             break
     print(f"Наименьший делитель, отличный от единицы: {result}")
 
 
-
 if n <= 1:
     print("Число меньше или равно единицы")
 else:
-    divider (n)
+    divider(n)
+
+# зачёт!
