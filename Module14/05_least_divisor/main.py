402f66d54ce0da15cc606ddd8f66e959b7e4448b
Ilnur <ilnrzakirov@gmail.com>
2021-01-16 17:57:16 +0300

    задача 5 решена

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+n = int(input("Введите число: "))
+
+def divider (n):
+    result = 0
+    for number in range (2, n + 1):
+        if n % number == 0:
+            result = number
+            break
+    print(f"Наименьший делитель, отличный от единицы: {result}")
+
+
+
+if n <= 1:
+    print("Число меньше или равно единицы")
+else:
+    divider (n)
