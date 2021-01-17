ae1dfdb36590be4ef9485d13b97e269d0fc55bdc
Ilnur <ilnrzakirov@gmail.com>
2021-01-17 19:14:14 +0300

    модуль 15 задача 10 решена

--- Module15/09_word_analysis_2/main.py
+++ Module15/09_word_analysis_2/main.py
@@ -1,16 +1,20 @@
 word = input("Введите слово: ")
 word_list = []
 remains = 0
-flag = True
+
 
 for symbol in word:
     word_list.append(symbol)
 # TODO, а как решить без индексов и срезов? Ведь если решить через них, то циклы не нужны =)
 # TODO, как в цикле записать слово наоборот? Помните задание с сэндвичем? =)
 
+revers = ""
+for symbol in word_list:
+    revers = symbol + revers
+print( revers)
 
 
-if flag == True:
+if word == revers:
     print("Слово является палиндромом")
 else:
     print("Слово не является палиндромом")
