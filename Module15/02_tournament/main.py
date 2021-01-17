e7c7899185f0166c9bbb92b9296aff8dd024fa5a
Insolente13 <lanc05@mail.ru>
2021-01-17 14:55:00 +0300

    Module 15 02, 03, 05 - ok, 01, 04 - TODO

--- Module15/02_tournament/main.py
+++ Module15/02_tournament/main.py
@@ -1,5 +1,7 @@
 people = ["Артемий", "Борис", "Влад", "Гоша", "Дима", "Евгений", "Женя", "Захар"]
 
-for num in range (8):
+for num in range(8):
     if num % 2 == 0:
         print(people[num])
+
+# зачёт!
--- Module15/03_cells/main.py
+++ Module15/03_cells/main.py
@@ -2,9 +2,11 @@ n = int(input("Введите количество клеток: "))
 cells = []
 marriage = ""
 
-for num in range (0, n):
-    print(f"Эффективность {num+1} клетки:", end=" ")
+for num in range(0, n):
+    print(f"Эффективность {num + 1} клетки:", end=" ")
     cells.append(int(input()))
     if num > cells[num]:
         marriage += " " + str(cells[num])
 print(f"Неподходящие значения: {marriage}")
+
+# зачёт!
