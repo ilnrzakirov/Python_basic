ae1d2f5f8f4e2883227bf12639ac54ef7cbb8b68
Insolente13 <lanc05@mail.ru>
2021-01-17 18:22:37 +0300

    Module 15 09, 10 - TODO

--- Module15/10_sort/main.py
+++ Module15/10_sort/main.py
@@ -1,31 +1,33 @@
-first_list = [1, 4, -3, 0, 10]
+first_list = [1, 4, -3, 0, 10, -125, -500, 17]  # TODO, добавил элементы в список и работать перестало =)
 min_list = min(first_list)
 items = len(first_list)
-flag = False
+flag = False  # TODO, переменная лишняя
 
-def sort (first_list):
-    for number in range (len(first_list) - 1) :
+
+def sort(first_list):
+    for number in range(len(first_list) - 1):
         first_item = first_list[number]
-        sescond_item = first_list [number + 1]
+        sescond_item = first_list[number + 1]
         if first_item > sescond_item:
             first_list[number], first_list[number + 1] = first_list[number + 1], first_list[number]
     return first_list
 
+
 first_list = sort(first_list)
 
-while flag != True:
+while flag != True:  # TODO, лучше без переменной.
     item = first_list[1]
     res = 0
-    for number in first_list [:-2]:
-        if item >= number:
+    for number in first_list[:-2]:
+        # Почем после цикла вайл программа снова запускает сортировку!
+        if item >= number:  # TODO, видимо из-за этого условия. Оно не срабатывает и мы переходим в else.
             item = first_list[number + 2]
-            res +=1
+            res += 1
         else:
             first_list = sort(first_list)
             res = 0
-        if res == len(first_list) -2:
+        if res == len(first_list) - 2:
             flag = True
             break
-
 
 print(first_list)
