a8ed5d1a5d6ee2afaa3d816039834eea845447aa
Ilnur <ilnrzakirov@gmail.com>
2021-01-17 13:20:01 +0300

    модуль 15 задача 3 решена

--- Module15/03_cells/main.py
+++ Module15/03_cells/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+n = int(input("Введите количество клеток: "))
+cells = []
+marriage = ""
+
+for num in range (0, n):
+    print(f"Эффективность {num+1} клетки:", end=" ")
+    cells.append(int(input()))
+    if num > cells[num]:
+        marriage += " " + str(cells[num])
+print(f"Неподходящие значения: {marriage}")
