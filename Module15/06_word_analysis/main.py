05c239a6088daaa1471f39ac5281e68408d4d04f
Insolente13 <lanc05@mail.ru>
2021-01-17 15:04:48 +0300

    Module 15 01, 04, 06 - ok

--- Module15/06_word_analysis/main.py
+++ Module15/06_word_analysis/main.py
@@ -1,14 +1,14 @@
 word = input("Введите слово: ")
 letters = []
-similar =[]
+similar = []
 n = len(word)
 result = []
 for symbol in word:
     letters.append(symbol)
 
-for item in range (n - 1):
-    for symbol in range (item + 1, n):
-        if letters[item] == letters [symbol]:
+for item in range(n - 1):
+    for symbol in range(item + 1, n):
+        if letters[item] == letters[symbol]:
             similar.append(letters[item])
             break
 
@@ -16,5 +16,6 @@ for item in letters:
     if item not in similar:
         result.append(item)
 
-
 print(f"Количество уникальных букв: {len(result)}")
+
+# зачёт!
