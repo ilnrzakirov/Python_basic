d5873c6675244cce1bcd43e3e6d6a7209cf751b1
Ilnur <ilnrzakirov@gmail.com>
2021-01-17 15:57:49 +0300

    модуль 15 задача 9 решена

--- Module15/09_word_analysis_2/main.py
+++ Module15/09_word_analysis_2/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+word = input("Введите слово: ")
+word_list = []
+remains = 0
+flag = True
+
+for symbol in word:
+    word_list.append(symbol)
+
+for symbol in word[::-1]:
+    if symbol != word[remains]:
+        flag = False
+        break
+    remains += 1
+
+if flag == True:
+    print("Слово является палиндромом")
+else:
+    print("Слово не является палиндромом")
