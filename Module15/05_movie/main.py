c1972705f65dc4ed2384785ca255ba5e9fd7fc86
Ilnur <ilnrzakirov@gmail.com>
2021-01-17 13:43:23 +0300

    модуль 15 задача 5 решена

--- Module15/05_movie/main.py
+++ Module15/05_movie/main.py
@@ -1,5 +1,15 @@
 films = ['Крепкий орешек', 'Назад в будущее', 'Таксист',
          'Леон', 'Богемская рапсодия', 'Город грехов',
          'Мементо', 'Отступники', 'Деревня']
+answer =""
+my_films = []
 
-# TODO здесь писать код
+while answer != "end":
+    answer = input("Введите название фильма: ")
+    if answer in films:
+        my_films.append(answer)
+    elif answer == "end":
+        break
+    elif answer not in films:
+        print("Этого фильма в списке рецензий нет ")
+print(f'Список любимых фильмов с рецензией {my_films}')
