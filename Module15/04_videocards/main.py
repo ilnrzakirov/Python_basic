b016b94130cdf4a1373848447dc24bfd9af4c831
Ilnur <ilnrzakirov@gmail.com>
2021-01-17 13:30:55 +0300

    модуль 15 задача 4 решена

--- Module15/04_videocards/main.py
+++ Module15/04_videocards/main.py
@@ -1,3 +1,17 @@
-# TODO здесь писать код
+n = int(input("Введите количество видекарт: "))
+video_card = []
+new_list_video_card = []
 
+for num in range(n):
+    print(f"{num + 1} Видеокарта: ", end=" ")
+    video_card.append(int(input()))
+
+max_video_card = max(video_card)
+
+for number in video_card:
+    if max_video_card != number:
+        new_list_video_card.append(number)
+
+print(f"Старый список видео карт {video_card}")
+print(f"Новый список видео карт {new_list_video_card}")
 
