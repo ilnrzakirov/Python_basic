a33c27ae6cd3f79415dce79726f2d3edfd953f4c
Ilnur <ilnrzakirov@gmail.com>
2021-01-17 15:20:44 +0300

    модуль 15 задача 7 решена

--- Module15/07_containers/main.py
+++ Module15/07_containers/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+n = int(input("Введите количество контейнеров: "))
+container = []
+result = 0
+for num in range (1, n +1):
+    container.append(int(input("Введите вес контейнера: ")))
+
+new_container = int(input("Введите вес нового контейнера: "))
+
+for item in container:
+    result += 1
+    if new_container > item:
+        break
+    if result == len(container):
+        result +=1
+        break
+
+print(result, container)
