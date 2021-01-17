fd00a1cfb133c36fd0031640595293ce61d3e2e3
Ilnur <ilnrzakirov@gmail.com>
2021-01-17 12:58:48 +0300

    модуль 15 задача 1 решена

--- Module15/01_list_gen/main.py
+++ Module15/01_list_gen/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+n = int(input("Введите число: "))
+number_list =[]
+
+for number in range (1, n + 1):
+    if number % 2 == 0:
+        number_list.append(number)
+
+print(number_list)
+
