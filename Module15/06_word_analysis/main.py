3be189386bcd43f4e8a2b01cabbe6766f9ef63bb
Ilnur <ilnrzakirov@gmail.com>
2021-01-17 14:57:04 +0300

    модуль 15 задача 6 решена

--- Module15/06_word_analysis/main.py
+++ Module15/06_word_analysis/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+word = input("Введите слово: ")
+letters = []
+similar =[]
+n = len(word)
+result = []
+for symbol in word:
+    letters.append(symbol)
+
+for item in range (n - 1):
+    for symbol in range (item + 1, n):
+        if letters[item] == letters [symbol]:
+            similar.append(letters[item])
+            break
+
+for item in letters:
+    if item not in similar:
+        result.append(item)
+
+
+print(f"Количество уникальных букв: {len(result)}")
