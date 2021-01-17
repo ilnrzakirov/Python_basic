5eeb069b45657f777e8220f2d8a1ef8d85c8d16c
Ilnur <ilnrzakirov@gmail.com>
2021-01-17 15:47:21 +0300

    модуль 15 задача 8 решена

--- Module15/08_running_nums/main.py
+++ Module15/08_running_nums/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+shift = int(input("Сдвиг: "))
+n = [1, 2, 3, 4, 5]
+new_list = []
+
+for symbol in n [:len(n) - shift -1:-1]:
+    new_list.append(symbol)
+
+for symbol in n [:len(n) - shift ]:
+    new_list.append(symbol)
+
+print(new_list)
