de1b3a954a69556f247fd3f1e4b7b344609cf665
Ilnur <ilnrzakirov@gmail.com>
2021-01-17 15:00:15 +0300

    модуль 15 задача 1, 4 исправил

--- Module15/01_list_gen/main.py
+++ Module15/01_list_gen/main.py
@@ -2,12 +2,8 @@ n = int(input("Введите число: "))
 number_list =[]
 
 for number in range (1, n + 1):
-    if number % 2 == 0:
+    if number % 2 != 0:
         number_list.append(number)
 
 print(number_list)
 
-# TODO Напишите программу, которая формирует список из нечетных чисел от 1 до N.
-#  Введите число: 11
-#  [2, 4, 6, 8, 10]
-#  Пока что все числа чётные. Должны закончить числом 11 =)
--- Module15/04_videocards/main.py
+++ Module15/04_videocards/main.py
@@ -3,8 +3,8 @@ video_card = []
 new_list_video_card = []
 
 # TODO, как исправить range таким образом, чтобы не делать вычисления с переменной цикла "num + 1" ? =)
-for num in range(n):
-    print(f"{num + 1} Видеокарта: ", end=" ")
+for num in range(1, n +1):
+    print(f"{num} Видеокарта: ", end=" ")
     video_card.append(int(input()))
 
 max_video_card = max(video_card)
