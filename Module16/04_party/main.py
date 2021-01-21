3a9953cf56f96b4780c653a13d967f78446cb45d
Ilnur <ilnrzakirov@gmail.com>
2021-01-21 16:47:07 +0300

    task_4

--- Module16/04_party/main.py
+++ Module16/04_party/main.py
@@ -1,3 +1,22 @@
 guests = ['Петя', 'Ваня', 'Саша', 'Лиза', 'Катя']
+all_guests = 5
+answer = ""
 
-# TODO здесь писать код
+while answer != "пора спать":
+    print(f"Сейчас на вечеринке {all_guests} человек. {guests}")
+    answer = input("Гость пришел или ушел: ")
+    if answer == "пришел":
+        name = input("Имя гостя: ")
+        if all_guests >= 6:
+            print(f"Прости, {name}, но мест больше нет")
+        else:
+            guests.append(name)
+            print(f"Привет, {name}")
+            all_guests += 1
+    elif answer == "ушел":
+        all_guests -= 1
+        name = input("Имя гостя: ")
+        guests.remove(name)
+        print(f"Пока, {name}")
+    if answer == "пора спать":
+        print("Вечеринка закончилась, все легли спать")
