223e82fb7bf2c208e29baf81d82f977b5bfcc643
Ilnur <ilnrzakirov@gmail.com>
2021-01-21 11:10:06 +0300

    task_1

--- Module16/01_scary_code/main.py
+++ Module16/01_scary_code/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+first_list = [1, 5, 3]
+second_list = [1, 5, 1, 5]
+third_list = [1, 3, 1, 5, 3, 3]
+
+first_list.extend(second_list)
+print(f"Количество цифр 5 при первом объединении: {first_list.count(5)}")
+
+for _ in range (3):
+    first_list.remove(5)
+first_list.extend(third_list)
+print(f"Количество цифр 3 при втором объединении: {first_list.count(3)}")
+
+
+print(first_list)
