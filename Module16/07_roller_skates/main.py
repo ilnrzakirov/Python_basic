74780f23490036585db6b9451d234598db7b8a8b
Ilnur <ilnrzakirov@gmail.com>
2021-01-22 11:28:05 +0300

    task_7

--- Module16/07_roller_skates/main.py
+++ Module16/07_roller_skates/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+skates = int(input("Количество коньков: "))
+all_skates = []
+result = 0
+
+for number in range (skates):
+    print(f"Размер {number + 1} пары: ", end=" ")
+    skate = int(input())
+    all_skates.append(skate)
+
+people = int(input("Количество людей: "))
+
+for number in range (people):
+    print(f"Размер ноги {number + 1} человека: ", end=" ")
+    size = int(input())
+    for i_team in all_skates:
+        if size <= i_team:
+            result += 1
+            all_skates.remove(i_team)
+            break
+
+print(f"Наибольшее количество людей, которые могут взять ролики: {result} ")
