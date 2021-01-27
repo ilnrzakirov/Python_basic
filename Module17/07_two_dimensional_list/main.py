b71053e9568da952aa2969483759878fadc4c4b2
Insolente13 <lanc05@mail.ru>
2021-01-27 21:44:11 +0300

    Module 17 07, 09 - ok, 08, 10 - TODO

--- Module17/07_two_dimensional_list/main.py
+++ Module17/07_two_dimensional_list/main.py
@@ -1,4 +1,6 @@
-frst_list = [[x for x in range (1, 10, 4) if x % 2 != 0], [x for x in range (2, 11 ,4) if x % 2 == 0],
-             [x for x in range (3, 12, 4) if x % 2 != 0 ], [x for x in range (4, 13, 4) if x % 2 == 0]
+frst_list = [[x for x in range(1, 10, 4) if x % 2 != 0], [x for x in range(2, 11, 4) if x % 2 == 0],
+             [x for x in range(3, 12, 4) if x % 2 != 0], [x for x in range(4, 13, 4) if x % 2 == 0]
              ]
 print(frst_list)
+
+# зачёт!
--- Module17/09_list_of_lists_2/main.py
+++ Module17/09_list_of_lists_2/main.py
@@ -1,8 +1,10 @@
 nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
              [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+#  здесь писать код
 
-new_list = [x for i in nice_list for i_team in i for x in i_team ]
+new_list = [x for i in nice_list for i_team in i for x in i_team]
 
 print(new_list)
+
+# зачёт!
