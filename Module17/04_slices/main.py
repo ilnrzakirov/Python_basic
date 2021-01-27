8d674da7a16c4eb8bd6fecdd28e8f54572a41149
Ilnur <ilnrzakirov@gmail.com>
2021-01-27 13:55:38 +0300

    task_4

--- Module17/04_slices/main.py
+++ Module17/04_slices/main.py
@@ -1,3 +1,14 @@
 alphabet = 'abcdefg'
 
-# TODO здесь писать код
+
+
+print(alphabet[:])
+print(alphabet[::-1])
+print(alphabet[::2])
+print(alphabet[1::2])
+print(alphabet[:1])
+print(alphabet[len(alphabet)-1:len(alphabet) - 2: -1])
+print(alphabet[3:4])
+print(alphabet[-3:])
+print(alphabet[3] + alphabet [4])
+print(alphabet[4] + alphabet [3])
