ddbe2d1d797ac740ad7e05509f2a4d4ab9d6077b
Ilnur <ilnrzakirov@gmail.com>
2021-01-28 12:15:44 +0300

    task_8

--- Module17/08_entertainment/main.py
+++ Module17/08_entertainment/main.py
@@ -1,1 +1,21 @@
 # TODO здесь писать код
+n = int(input("Введите количество палочек: "))
+stick_list = [x for x in range(1, n + 1)]
+
+throws = int(input("Количество бросков: "))
+
+for i_throws in range (throws):
+    print(f"Бросок {i_throws + 1}")
+    left = int(input("Сбиты палки с номера  "))
+    rigth = int(input("по номер  "))
+    for i_border in range (left, rigth + 1):
+        stick_list [i_border - 1] = ["."]
+
+for symbol in stick_list:
+    if symbol != ["."]:
+        ind = stick_list.index(symbol)
+        stick_list [ind] = ["|"]
+
+
+
+print(stick_list)
