2bc59ca5487beb88f4e0bc3f72988b2d0d4c55de
Ilnur <ilnrzakirov@gmail.com>
2021-01-28 11:26:36 +0300

    task_6

--- Module17/06_list_compression/main.py
+++ Module17/06_list_compression/main.py
@@ -5,12 +5,17 @@ def appnd(num):
     for _ in range(num):
         all_list1.append(0)
 
+def rem (x):
+    for _ in range (x):
+        all_list1.remove(0)
 
 all_list1 = [i_team for i_team in all_list if i_team != 0]
 remains = len(all_list) - len(all_list1)
 appnd(remains)
-
 print(all_list1)
-print(all_list1[:len(all_list1) - remains])
+x = all_list1.count(0)
+rem(x)
+print(all_list1)
+
 
 # TODO, у как удалить "0" из списка списковым методом?
--- Module17/10_caesar_cipher/main.py
+++ Module17/10_caesar_cipher/main.py
@@ -1,1 +1,20 @@
 # TODO здесь писать код
+l = ['а','б','в','г','д','е','ж','з','и','й','к','л','м','н','о','п','р',
+        'с','т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю','я']
+text = input("Введите сообщение: ")
+shift = int(input("Сдвиг: "))
+encrypted = ""
+
+for symbol in text:
+    if symbol != " ":
+        i_symbol = l.index(symbol)
+        if i_symbol + shift >= len(l):
+            remains = len(l) - (i_symbol) + 1
+            shift = remains - shift
+            encrypted += l[i_symbol + abs(shift)]
+            continue
+
+        encrypted += l[i_symbol + shift]
+
+
+print(encrypted)
