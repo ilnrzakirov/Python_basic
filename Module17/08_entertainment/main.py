0fd9ef73761b097a19f5208a94ea5acfaae16564
Insolente13 <lanc05@mail.ru>
2021-01-29 23:09:54 +0300

    Module 17 ok

--- Module17/08_entertainment/main.py
+++ Module17/08_entertainment/main.py
@@ -1,23 +1,23 @@
-# TODO здесь писать код
+#  здесь писать код
 n = int(input("Введите количество палочек: "))
 stick_list = [x for x in range(1, n + 1)]
 
 throws = int(input("Количество бросков: "))
 
-for i_throws in range (throws):
+for i_throws in range(throws):
     print(f"Бросок {i_throws + 1}")
     left = int(input("Сбиты палки с номера  "))
     rigth = int(input("по номер  "))
-    for i_border in range (left, rigth + 1):
-        stick_list [i_border - 1] = "."
+    for i_border in range(left, rigth + 1):
+        stick_list[i_border - 1] = "."
 
 for symbol in stick_list:
     if symbol != ".":
         ind = stick_list.index(symbol)
-        stick_list [ind] = "I"
+        stick_list[ind] = "I"
 
-
-# TODO как вывести результат в формате "Результат: I.....I..." ?
-for x in range (len(stick_list)):
+#  как вывести результат в формате "Результат: I.....I..." ?
+for x in range(len(stick_list)):
     print(stick_list[x], end="")
 
+# зачёт!
