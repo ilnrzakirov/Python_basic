bcd48d5aa5b45f0568bc9f084977726cce8a0944
Ilnur <ilnrzakirov@gmail.com>
2021-02-08 16:57:15 +0300

    task_5

--- Module18/05_captain_Flint/main.py
+++ Module18/05_captain_Flint/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+y = input("По оси ОУ: ")
+x = input("По оси ОХ: ")
+
+if y.startswith("South"):
+    y_list = y.split(" ")
+    coordinate_y = "-" + str(y_list[1])
+else:
+    remains = y.index(" ")
+    coordinate_y = y[remains + 1:]
+
+if x.startswith("East"):
+    remains1 = x.index(" ")
+    coordinate_x = x[remains1 + 1:]
+else:
+    x_list = x.split(" ")
+    coordinate_x = "-" + str(x_list[1])
+
+print("Координаты {} {}".format(coordinate_x, coordinate_y))
