1555cdbbbbf6b5b01020d737a406b38460a8a825
Ilnur <ilnrzakirov@gmail.com>
2021-02-08 17:52:38 +0300

    task_9

--- Module18/09_ip_adress_2/main.py
+++ Module18/09_ip_adress_2/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+ip = input("Введите IP адресс: ").split(".")
+for number in ip:
+    if not number.isdigit():
+        print(number, "не целое число")
+        break
+    elif int(number) > 255:
+        print(number, "превышает 255")
+        break
+    elif len(ip) != 4:
+        print("Адресс - это четыре числа, разделенные точками ")
+        break
+else:
+    print("IP адресс корректен ")
