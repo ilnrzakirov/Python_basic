7cae75c69042dd461e5c6148e967620550bcdfed
Ilnur <ilnrzakirov@gmail.com>
2021-02-09 16:12:53 +0300

    task_12

--- Module18/13_anagram/main.py
+++ Module18/13_anagram/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+first_word = input("Введите первое слово: ")
+second_word = input("Введите второе слово: ")
+chek = True
+
+for symbol in first_word:
+    if not second_word.count(symbol):
+        print("Слова не являются анаграммами друг друга")
+        chek = False
+        break
+    else:
+
+
+
+if chek == True:
+    print("Слова являются анаграммами друг друга")
