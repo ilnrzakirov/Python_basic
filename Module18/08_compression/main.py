f51eea9507efdc3a310b3054e3e01fe834af3283
Ilnur <ilnrzakirov@gmail.com>
2021-02-09 12:33:31 +0300

    task_8

--- Module18/08_compression/main.py
+++ Module18/08_compression/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+text = input("Введите строку: ")
+symbol_str = ""
+symbol = ""
+
+for _ in range (len(text)):
+    for number in range(len(text), 0, - 1):
+        if number != 0:
+            symbol = text[0] * number
+        else:
+            symbol = text[0]
+        if text.startswith(symbol):
+            symbol_str += text[0] + str(number)
+            text = text [number : ]
+            break
+
+print(symbol_str)
