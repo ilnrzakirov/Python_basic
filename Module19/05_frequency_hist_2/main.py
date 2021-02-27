0af6c5c0a4c33bc4cb5c6cdf967a8fbcc1059257
Ilnur <ilnrzakirov@gmail.com>
2021-02-27 20:42:54 +0300

    task_5

--- Module19/05_frequency_hist_2/main.py
+++ Module19/05_frequency_hist_2/main.py
@@ -12,7 +12,7 @@ text = input("Введите текст: ").lower()
 hist = histogram(text)
 
 print("Оригинальный словарь частот: ")
-for i_sym in sorted(hist.keys()):  # Можно без .keys()
+for i_sym in sorted(hist):  # Можно без .keys()
     print(i_sym, ":", hist[i_sym])
 
 hist_val = hist.values()
@@ -21,15 +21,16 @@ hist_key = hist.keys()
 new_hist = dict()
 count = 0
 for i_key in hist_key:
+    hist_list_key = hist[i_key]
     # Возможно, стоит создать переменную для hist[i_key] и далее, работать с ней =)
-    if hist[i_key] in new_hist:
-        new_hist[hist[i_key]].append(i_key)
+    if hist_list_key in new_hist:
+        new_hist[hist_list_key].append(i_key)
     else:
-        new_hist[hist[i_key]] = []
-        new_hist[hist[i_key]].append(i_key)
+        new_hist[hist_list_key] = []
+        new_hist[hist_list_key].append(i_key)
 
 print("Инвертированный словарь частот: ")
-for i_sym in sorted(new_hist.keys()):  # Можно без .keys()
+for i_sym in sorted(new_hist):  # Можно без .keys()
     print(i_sym, ":", new_hist[i_sym])
 
 # зачёт!
