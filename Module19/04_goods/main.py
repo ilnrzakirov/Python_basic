e230be858d05832debc381136a3d8d840cb67b1f
Insolente13 <lanc05@mail.ru>
2021-02-27 16:48:14 +0300

    Module 19 04 - TODO

--- Module19/04_goods/main.py
+++ Module19/04_goods/main.py
@@ -24,8 +24,7 @@ store = {
     ],
 }
 
-
-# TODO, Ильнур, ответ верный, давайте поработаем над стилем.
+# , Ильнур, ответ верный, давайте поработаем над стилем.
 #  Предлагаю для store[goods['Стол']] и т.д. придумать переменные и использовать их в вычислениях,
 #  в таком случае, кода станет меньше. И пропадёт большое кол-во [[[]]].
 table = store[goods['Стол']]
@@ -33,15 +32,20 @@ lamp = store[goods["Лампа"]]
 sofa = store[goods['Диван']]
 chair = store[goods['Стул']]
 lamp_total_price = lamp[0]["quantity"] * lamp[0]["price"]
+# TODO, в коде есть небольшая опечатка, в связи с этим, итоговая сумма столов пока что выводится некорректно.
 table_total_quantity = table[0]["quantity"] + table[1]["quantity"]
 table_total_price = table[0]["quantity"] * table[0]["price"]
 sofa_total_quantity = sofa[0]["quantity"] + sofa[1]["quantity"]
-sofa_total_price = sofa[0]["quantity"] * sofa [0]["price"] + sofa[1]["quantity"] * sofa[1]["price"]
+sofa_total_price = sofa[0]["quantity"] * sofa[0]["price"] + sofa[1]["quantity"] * sofa[1]["price"]
 chair_total_quantity = chair[0]["quantity"] + chair[1]["quantity"] + chair[2]["quantity"]
-chair_total_price = chair[0]["quantity"] * chair[0]["price"] + chair[1]["quantity"] * chair[1]["price"] + \
-                    chair [2]["quantity"] * chair[2]["price"]
+# Пожалуйста, обратите внимание на перенос строки при больших вычислениях.
+chair_total_price = (
+        chair[0]["quantity"] * chair[0]["price"]
+        + chair[1]["quantity"] * chair[1]["price"]
+        + chair[2]["quantity"] * chair[2]["price"]
+)
 
-# TODO, Делать большое количество вычислений в print не очень хорошо.
+# , Делать большое количество вычислений в print не очень хорошо.
 #  Предлагаю создать дополнительные переменные для вычислений и уже их вывести при помощи print.
 
 print("Лампа -", lamp[0]["quantity"], "шт, стоимость", lamp_total_price, "руб.")
@@ -49,3 +53,5 @@ print("Стол - ", table_total_quantity, "шт, стоимость", table_tot
 print("Диван - ", sofa_total_quantity, "шт, стоимость", sofa_total_price, "руб.")
 print("Стул - ", chair_total_quantity,
       "штб стоимость", chair_total_price, "руб.")
+
+
