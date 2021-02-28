71d6278e10dff58b5207e7030600638ae2145eb0
Ilnur <ilnrzakirov@gmail.com>
2021-02-28 10:53:56 +0300

    task_6

--- Module19/06_synonym_dict/main.py
+++ Module19/06_synonym_dict/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+num_pairs = int(input("Введите количество пар слов: "))
+pairs_dict = dict ()
+pairs_dict_rev = dict ()
+
+for i_pairs in range (1, num_pairs + 1):
+    print("{} пара: ".format(i_pairs), end=" ")
+    pair = input().lower().split(" - ")
+    pairs_dict[pair[0]] = pair[1]
+    pairs_dict_rev[pair[1]] = pair[0]
+
+flag = False
+while flag == False:
+    word = input("Введите слово: ").lower()
+    if word in pairs_dict:
+        print("Синоним {}".format(pairs_dict[word]))
+        flag = True
+    elif word in pairs_dict_rev:
+        print("Синоним {}".format(pairs_dict_rev[word]))
+        flag = True
+    else:
+        print("Такого слова в словаре нет")
