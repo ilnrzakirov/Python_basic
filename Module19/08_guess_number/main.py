fde7f14bbcf9617b60fc1720a6bb5156cfc32015
Ilnur <ilnrzakirov@gmail.com>
2021-02-28 12:21:29 +0300

    task_8

--- Module19/08_guess_number/main.py
+++ Module19/08_guess_number/main.py
@@ -1,1 +1,27 @@
-# TODO здесь писать код
+max_number = int(input("Введите макисмальное число: "))
+
+answer_boris = ""
+answer_dict = dict ()
+while answer_boris != "Помогите!":
+    answer_boris = input("Нужное число есть среди вот этих чисел: ")
+    if answer_boris == "Помогите!":
+        break
+    answer_artem = input("Ответ Артема: ")
+    if answer_artem == "Да":
+        if "Да" in answer_dict:
+            answer_dict["Да"].append(answer_boris.split())
+        else:
+            answer_dict["Да"] = answer_boris.split()
+    elif answer_artem == "Нет":
+        if "Нет" in answer_dict:
+            answer_dict["Нет"].append(answer_boris.split())
+        else:
+            answer_dict["Нет"] = answer_boris.split()
+
+for sym in answer_dict["Нет"]:
+    if sym in answer_dict["Да"]:
+        answer_dict["Да"].remove(sym)
+
+print('Артем мог загадать числа: ', end="")
+for sym in answer_dict["Да"]:
+    print(sym, end=" ")
