11ae2a2ca7533554fa4870ed6252b4116e309862
Ilnur <ilnrzakirov@gmail.com>
2021-02-28 15:35:05 +0300

    task_10

--- Module19/10_palindrome_again/main.py
+++ Module19/10_palindrome_again/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+string = input("Введите строку: ")
+
+nok = 0
+ok = 0
+remains = 1
+for sym in string:
+    if sym not in string [remains:]:
+        nok +=1
+    else:
+        ok += 1
+    remains += 1
+
+if nok - ok > 1:
+    print("Нельзя сделать палиндром")
+else:
+    print('Можно сделать палиндром')
