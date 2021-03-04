b15be50bbb7c455b82f0040386046d670a41bcd8
Ilnur <ilnrzakirov@gmail.com>
2021-03-04 22:57:38 +0300

    task_10

--- Module20/10_my_zip/main.py
+++ Module20/10_my_zip/main.py
@@ -2,7 +2,7 @@ def great_tuple (element1, element2):
     return element1, element2
 
 my_string = "abcd"
-my_tuple = (10, 20, 30, 40)
+my_tuple = ("d", 20, 30, 40)
 
 if len(my_string) <= len(my_tuple):
     for i_item, item in enumerate(my_string):
