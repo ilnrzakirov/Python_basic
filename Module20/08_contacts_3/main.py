2149b781761a866a896a920ee64621da7e54017b
Ilnur <ilnrzakirov@gmail.com>
2021-03-04 20:02:31 +0300

    task_8

--- Module20/08_contacts_3/main.py
+++ Module20/08_contacts_3/main.py
@@ -1,1 +1,30 @@
-# TODO здесь писать код
+
+
+phonebook = {
+    1: { "Имя" : "Иван",
+         "Фамилия" : "Сидоров",
+         "Номер" : 899999999
+    },
+    2 : { "Имя" : "Руслан",
+          "Фамилия" : "Петров",
+          "Номер" : 89995659
+    },
+}
+
+while True:
+    print("1 - Что бы добавить контакт, 2 - Найти контакт по фамилии")
+    answer = int(input())
+    if answer == 1:
+        name = input("Введите имя: ")
+        surname = input("Введите фамилию: ")
+        tel = int(input("Введите номер телефона: "))
+        id = len(phonebook) + 1
+        phonebook[id] = {"Имя" : name,
+                         "Фамилия" : surname,
+                         "Номер" : tel}
+    if answer == 2:
+        answer = input("Введите фамилию для поиска: ").capitalize()
+        for  i_key, i_value in phonebook.items():
+            if answer == phonebook[i_key]["Фамилия"]:
+                contact = phonebook[i_key]
+                print(contact["Имя"], contact["Фамилия"], contact["Номер"])
