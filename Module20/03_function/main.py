b09b97b4943f78d6b5c225efe8873ef518025e65
Ilnur <ilnrzakirov@gmail.com>
2021-03-04 13:39:25 +0300

    task_3

--- Module20/03_function/main.py
+++ Module20/03_function/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+def selection (tuple_elements, element):
+    new_item_list = []
+    chek = False
+    for item in tuple_elements:
+        if element in new_item_list and item == element:
+            new_item_list.append(item)
+            break
+        if item == element:
+            chek = True
+        if chek:
+            new_item_list.append(item)
+    new_item_tuple = tuple(new_item_list)
+    return new_item_tuple
+
+tuple_elements = (1, 2, 3, 4, 5, 2, 8)
+element = 2
+print(selection(tuple_elements, element))
+
