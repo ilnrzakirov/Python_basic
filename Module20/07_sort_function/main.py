9d4756c0b3906eb2207cfd94bbd09f93a540e3ba
Ilnur <ilnrzakirov@gmail.com>
2021-03-04 19:12:05 +0300

    task_10

--- Module20/07_sort_function/main.py
+++ Module20/07_sort_function/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+def sort (sort_tuple):
+    for item in sort_tuple:
+        if not str(item).isdigit():
+            return sort_tuple
+    sort_tuple = list(sort_tuple)
+    chek = True
+    while chek:
+        for i_item ,item in enumerate(sort_tuple):
+            if i_item == len(sort_tuple)- 1:
+                break
+            if item > sort_tuple[i_item + 1]:
+                sort_tuple [i_item], sort_tuple [i_item + 1] = sort_tuple [i_item + 1], sort_tuple [i_item]
+        # проверка
+
+    return sort_tuple
+
+my_tuple = (5, 4, 2, 6, 11, 9, 1)
+
+print(sort(my_tuple))
--- Module20/10_my_zip/main.py
+++ Module20/10_my_zip/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+def great_tuple (element1, element2):
+    return element1, element2
+
+my_string = "abcd"
+my_tuple = (10, 20, 30, 40)
+
+if len(my_string) <= len(my_tuple):
+    for i_item, item in enumerate(my_string):
+        print(great_tuple(item, my_tuple[i_item]))
+else:
+    for i_item, item in enumerate(my_tuple):
+        print(great_tuple(item, my_string[i_item]))
