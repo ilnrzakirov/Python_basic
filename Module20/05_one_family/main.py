1346213a8dcd22a843dd2848c249387e4a07f947
Ilnur <ilnrzakirov@gmail.com>
2021-03-04 19:27:51 +0300

    task_5

--- Module20/05_one_family/main.py
+++ Module20/05_one_family/main.py
@@ -5,6 +5,10 @@ people = {
     "Петров Павел" : 40
 }
 
-surname = input("Введите фамилию: ")
+surname = input("Введите фамилию: ").capitalize()
+if surname.endswith("а"):
+    surname = surname [:-2]
 
 for i_key, i_value in people.items():
+    if surname in i_key:
+        print(i_key, i_value)
