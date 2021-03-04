b21e919de66ba700cb0c042578d479a80b6cf483
Ilnur <ilnrzakirov@gmail.com>
2021-03-04 14:05:38 +0300

    task_4

--- Module20/04_players/main.py
+++ Module20/04_players/main.py
@@ -4,4 +4,17 @@ players = {
     ("Rob", "Bobbin"): (12, 8, 2)
 }
 
-# TODO здесь писать код
+players_list = []
+player = []
+for i_key, i_value in players.items():
+    for item in i_key:
+        player.append(item)
+    for item in i_value:
+        player.append(item)
+    player_tuple = tuple(player)
+    players_list.append(player_tuple)
+    player =[]
+
+
+#players_list = [item for item in players.items()]
+print(players_list)
