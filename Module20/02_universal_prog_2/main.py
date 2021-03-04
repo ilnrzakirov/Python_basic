c8671f376aa43b95de7e3657190a3b4b6560490b
Ilnur <ilnrzakirov@gmail.com>
2021-03-04 12:03:41 +0300

    task_2

--- Module20/02_universal_prog_2/main.py
+++ Module20/02_universal_prog_2/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+def is_prime (number):
+    chek = True
+    for num in range (2, number -1):
+        if number % num == 0:
+            chek = False
+    return chek
+
+def result_list (object):
+    result = []
+    for i_object, value in enumerate(object):
+        if is_prime(i_object):
+            result.append(value)
+
+    return result
+
