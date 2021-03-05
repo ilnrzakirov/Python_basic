6a9167eb35fcd02b3554db325a28e3b9b120b4fc
Ilnur <ilnrzakirov@gmail.com>
2021-03-05 11:11:27 +0300

    task_7

--- Module20/07_sort_function/main.py
+++ Module20/07_sort_function/main.py
@@ -1,28 +1,13 @@
 def sort(sort_tuple):
-    for item in sort_tuple:
-        # TODO, возможно стоит проверить тип элемента при помощи isinstance?
-        if not str(item).isdigit():
-            return sort_tuple
+    if not isinstance(sort_tuple, tuple):
+#    for item in sort_tuple:
+       # TODO, возможно стоит проверить тип элемента при помощи isinstance?
+#        if not str(item).isdigit():
+        return sort_tuple
     # TODO, возможно, весь остальной код лишний =)
     #  Можно применить в sort_tuple функцию sorted и вернуть =)
-    sort_tuple = list(sort_tuple)
-    chek = True
-    while chek:
-        cnt = 0
-        for i_item, item in enumerate(sort_tuple):
-            if i_item == len(sort_tuple) - 1:
-                break
-            if item > sort_tuple[i_item + 1]:
-                sort_tuple[i_item], sort_tuple[i_item + 1] = sort_tuple[i_item + 1], sort_tuple[i_item]
-        for i_item, item in enumerate(sort_tuple):
-            if i_item == len(sort_tuple) - 1:
-                break
-            if item < sort_tuple[i_item + 1]:
-                cnt += 1
-            if cnt == len(sort_tuple) - 1:
-                chek = False
-
-    return sort_tuple
+    else:
+        return sorted(sort_tuple)
 
 
 # my_tuple = (5, 4, 2, 6, 11, 9, 1)
