c5ca13da204dd92b5b62c1fbd875aa5259c0fb7e
Insolente13 <lanc05@mail.ru>
2021-03-05 21:29:15 +0300

    Module 20 07 - ok, 04 - TODO

--- Module20/07_sort_function/main.py
+++ Module20/07_sort_function/main.py
@@ -1,16 +1,18 @@
 def sort(sort_tuple):
     if not isinstance(sort_tuple, tuple):
-#    for item in sort_tuple:
-       # TODO, возможно стоит проверить тип элемента при помощи isinstance?
-#        if not str(item).isdigit():
+        #    for item in sort_tuple:
+        # , возможно стоит проверить тип элемента при помощи isinstance?
+        #        if not str(item).isdigit():
         return sort_tuple
-    # TODO, возможно, весь остальной код лишний =)
+    # , возможно, весь остальной код лишний =)
     #  Можно применить в sort_tuple функцию sorted и вернуть =)
     else:
         return sorted(sort_tuple)
 
 
 # my_tuple = (5, 4, 2, 6, 11, 9, 1)
-# TODO, пока что не сортируем элементы =)
+# , пока что не сортируем элементы =)
 my_tuple = (6, 3, -1, 8, 4, 10, -5)
 print(sort(my_tuple))
+
+# зачёт!
