34e4d69f73d584de31f0d3a6e84b2d99f2e5550a
Ilnur <ilnrzakirov@gmail.com>
2021-03-12 13:10:07 +0300

    task_5

--- Module21/05_make_function_faster/main.py
+++ Module21/05_make_function_faster/main.py
@@ -1,9 +1,15 @@
-def calculating_math_func(data):
+def calculating_math_func(data, fact_dict={0 : 1}):
     result = 1
-    for index in range(1, data + 1):
-        result *= index
+    if data in fact_dict:
+        result = fact_dict[data]
+    else:
+        for index in range(1, data + 1):
+            result *= index
+    fact_dict[data] = result
     result /= data ** 3
     result = result ** 10
     return result
 
 # TODO оптимизировать функцию
+
+print(calculating_math_func(8))
