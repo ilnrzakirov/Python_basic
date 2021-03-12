69f6ebe362f9cda1f59f170e16c830dd0e4d943e
Ilnur <ilnrzakirov@gmail.com>
2021-03-12 11:08:53 +0300

    task_1

--- Module21/01_challenge_2/main.py
+++ Module21/01_challenge_2/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+def output_num (num, step = 1):
+    if step == num:
+        return 1
+    print(num - (num - step))
+    return output_num(num, step +1)
+
+
+
+
+
+
+numm = output_num(5)
