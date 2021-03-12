124a8e38d6c00c4135008e0e1de6c1cf10707540
Ilnur <ilnrzakirov@gmail.com>
2021-03-12 18:23:18 +0300

    task_9

--- Module21/09_hanoi_towers/main.py
+++ Module21/09_hanoi_towers/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+def move (kernel1, kernel2, kernel3, n ):
+    for disk in kernel1:
+        if disk == 1:
+            kernel2.append(disk)
+            kernel1.remove(1)
+
+
+
+#n = int(input("Введите количество дисков: "))
+n = 5
+kernel1 = [number for number in range (n, 0, -1)]
+kernel2 = []
+kernel3 = []
+move(kernel1, kernel2, kernel3, n)
+
+print(kernel1)
+print(kernel2)
+print(kernel3)
