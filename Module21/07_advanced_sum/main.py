d9de811c83516d9db20972408a13486f5fb2cbc1
Ilnur <ilnrzakirov@gmail.com>
2021-03-12 15:46:58 +0300

    task_6

--- Module21/07_advanced_sum/main.py
+++ Module21/07_advanced_sum/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+def summ (object, result= 0):
+    for item in object:
+        if isinstance(item, list):
+            summ(item, result)
+        else:
+            result += item
+    return result
+
+# Обьясните пожалуйста, почему после возврата result = 11 с рекурсии, функция не обращает на это внимание, а
+# пользуется предыдущим значением = 6. И получается так, что недосчитывает значение с подсписков.
+
+my_list = [1, 2, 3, [5], 8, [1, 2]]
+print(summ(my_list))
