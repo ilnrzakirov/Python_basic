03df552d2c0a0dbb00505a8b3e4ce9ad62f17fd2
Ilnur <ilnrzakirov@gmail.com>
2021-03-12 12:25:20 +0300

    task_3

--- Module21/03_fibonacci/main.py
+++ Module21/03_fibonacci/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+def fibonachi (num_pos, chek = 2, start = 0, res = 1):
+    if num_pos == 1:
+        return 1
+    if chek == num_pos:
+        return res + start
+    chek +=1
+    result = res + start
+    start = res
+    return fibonachi(num_pos, chek, start, result)
+
+num_pos = int(input("Введите номер позиции: "))
+
+print(fibonachi(num_pos))
+
