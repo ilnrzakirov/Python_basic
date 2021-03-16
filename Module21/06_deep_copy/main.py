f32a45fce693337b0d3de02b480a29e53774489f
Ilnur <ilnrzakirov@gmail.com>
2021-03-16 22:21:53 +0300

    task6

--- Module21/06_deep_copy/main.py
+++ Module21/06_deep_copy/main.py
@@ -12,19 +12,16 @@ site = {
 }
 
 def constructor (struct, n):
-    if n == 1:
+    if n == 0:
         return None
     name = input("Введите название продукта для нового сайта: ")
     site["html"]["head"]["title"] = "Куплю/продам {} недорого".format(name)
     site["html"]["body"]["h2"] = "У нас самая низкая цена на {}".format(name)
-    print(struct)
-    return constructor(struct, n -1)
+    return print(struct), constructor(struct, n -1)
 
 
 
 
 n = int(input("Сколько сайтов: "))
-for _ in range (n):
-    name = input("Введите название продукта для нового сайта: ")
 constructor(site, n)
 
