90207309ffe5d87850ffe9f004eaa9d129752edc
Ilnur <ilnrzakirov@gmail.com>
2021-03-16 10:25:31 +0300

    task_2

--- Module21/02_my_zip_2/main.py
+++ Module21/02_my_zip_2/main.py
@@ -1,19 +1,19 @@
-def max_len (object1, object2):
+def min_len (object1, object2):
     return min(len(object1), len(object2))
 
 def generator (object1, object2):
-    result = ((object1[i_elem], object2[i_elem]) for i_elem in range (max_len(object1,object2)))
+    result = ((object1[i_elem], object2[i_elem]) for i_elem in range (min_len(object1,object2)))
     return result
 
 #def print_res_gen (object):
 
 
-lst = [3, 5, 6, 1, 5]
-tpl = (5, 4 , 3, 7)
-str = "avddgddfd"
+my_list = [3, 5, 6, 1, 5]
+my_tupl = (5, 4 , 3, 7)
+my_string = "avddgddfd"
 
 
-result = generator(lst, str)
+result = generator(my_list, my_string)
 #print_res_gen(result)
 print(result)
 for sym in result:
