1ebd847f50cb024e1c130c9f3efa6a41988ddc9c
Ilnur <ilnrzakirov@gmail.com>
2021-03-16 14:16:46 +0300

    task_8

--- Module21/08_list_of_lists_2/main.py
+++ Module21/08_list_of_lists_2/main.py
@@ -3,14 +3,20 @@ nice_list = [1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]],
 
 
 # TODO, пожалуйста, придумайте более полное название для переменной.
-def exp (lst, result_list = []): # TODO, параметр выгядит лишним. =)
-    # TODO, цикл лишний, в нашем случае, цикл заменяет рекурсия.
-    for item in lst:
-        if isinstance(item, list):
-            exp(item)
-        else:
-            result_list.append(item)
-    return result_list
+#def exp_list (input_list, result_list = []): # TODO, параметр выгядит лишним. =)
+#    # TODO, цикл лишний, в нашем случае, цикл заменяет рекурсия.
+#    for item in input_list:
+#        if isinstance(item, list):
+#            exp_list(item)
+#        else:
+#            result_list.append(item)
+#    return result_list
 
+def exp_list (input_list):
+    if input_list == []:
+        return input_list
+    if isinstance(input_list[0], list):
+        return exp_list(input_list[0]) + exp_list(input_list[1:])
+    return input_list[:1] + exp_list(input_list[1:])
 
-print(exp(nice_list))
+print(exp_list(nice_list))
