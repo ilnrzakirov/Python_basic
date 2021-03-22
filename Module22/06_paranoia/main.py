59e781c69c5816fa7298b80477609700805a1809
Ilnur <ilnrzakirov@gmail.com>
2021-03-22 17:04:54 +0300

    task6

--- Module22/06_paranoia/main.py
+++ Module22/06_paranoia/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+import os
+
+def chiper (text, shift):
+    char_list = [(alphabet[(alphabet.index(sym) + shift) % 26] if sym != "\n" else sym) for sym in text.lower()]
+    new_text = ''
+    for i_char in char_list:
+        new_text += i_char
+    return new_text
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+my_text_file = open("text.txt", 'r')
+chipe_text = open("chiper_text.txt", "a")
+shift = 0
+
+for i_line in my_text_file:
+    shift += 1
+    string = chiper(i_line, shift)
+    chipe_text.write(string)
+
+my_text_file.close()
+chipe_text.close()
