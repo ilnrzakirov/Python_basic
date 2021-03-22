0f5658eead8014c9889d744caa0fa9fc8d42f4b2
Ilnur <ilnrzakirov@gmail.com>
2021-03-22 11:08:37 +0300

    task1

--- Module22/01_nums_sum_2/main.py
+++ Module22/01_nums_sum_2/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+input_file = open("numbers.txt", "r")
+res_num = 0
+
+for i_line in input_file:
+    for i_elem in i_line:
+        if i_elem != " " and i_elem != "\n":
+            res_num += int(i_elem)
+
+answer = open("answer.txt", "w")
+answer.write(str(res_num))
+
+
+input_file.close()
+answer.close()
