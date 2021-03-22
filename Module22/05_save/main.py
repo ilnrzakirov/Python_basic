6ee5542ff949864ae04122b9602baf31c3da64c5
Ilnur <ilnrzakirov@gmail.com>
2021-03-22 22:37:24 +0300

    task5

--- Module22/05_save/main.py
+++ Module22/05_save/main.py
@@ -2,13 +2,14 @@ import os
 
 
 def save_document(string):
-    user_path = input("Куда хотите сохранить документ? Введите последовательность папок (через пробел)").split()
+    chek = False
+    while chek == False:
+        user_path = input("Куда хотите сохранить документ? Введите последовательность папок (через пробел)").split()
     #    user_path = ["users", "ilnur", "pycharmprojects", "python_basic", "module22", "05_save"]
-    print(user_path)
-    path = os.path.abspath(os.path.sep)
-    for elem in user_path:
-        path = os.path.join(path, elem)
-    print(path)
+        path = os.path.abspath(os.path.sep)
+        for elem in user_path:
+            path = os.path.join(path, elem)
+        chek = os.path.exists(path)
     file_name = input("Введите название файла: ")
     #    file_name = "my_document.txt"
     if file_name in os.listdir(path):
