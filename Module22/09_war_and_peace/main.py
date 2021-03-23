32a2c14413cccccbc0470c86c3a21c2dc8a23a0c
Ilnur <ilnrzakirov@gmail.com>
2021-03-23 11:34:08 +0300

    task9

--- Module22/09_war_and_peace/main.py
+++ Module22/09_war_and_peace/main.py
@@ -1,1 +1,44 @@
-# TODO здесь писать код
+import zipfile
+
+voyna_i_mir = zipfile.ZipFile("voyna-i-mir.zip", "r")
+voyna_i_mir.printdir()
+text_document = open(voyna_i_mir.extract("voyna-i-mir.txt"), "r", encoding="UTF-8")
+
+alfabit = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZабвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+sym_dict = dict()
+
+for i_line in text_document:
+    for sym in i_line:
+        if sym in alfabit:
+            if sym in sym_dict:
+                sym_dict[sym] += 1
+            else:
+                sym_dict[sym] = 1
+
+share_dict = dict()
+for name, value in sym_dict.items():
+    share = value
+    if share in share_dict:
+        share_dict[share].append(name)
+    else:
+        share_dict[share] = []
+        share_dict[share].append(name)
+
+key_sorted = sorted(share_dict)
+sorted_share_dict = dict()
+for key in key_sorted:
+    sorted_share_dict[key] = share_dict[key]
+
+for key, value in sorted_share_dict.items():
+    sorted_share_dict[key] = sorted(value)
+
+analysis_file = open("analysis.txt", "a")
+
+for key, value in sorted_share_dict.items():
+    for item in value:
+        write_string = item + " " + str(key) + "\n"
+        analysis_file.write(write_string)
+
+analysis_file.close()
+text_document.close()
+voyna_i_mir.close()
