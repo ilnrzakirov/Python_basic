756e1c3496949aec3ecbf6a788ba39e9d23dd166
Ilnur <ilnrzakirov@gmail.com>
2021-03-23 21:55:06 +0300

    task7

--- Module22/07_tournament/main.py
+++ Module22/07_tournament/main.py
@@ -11,13 +11,11 @@ for i_line in input_file.readlines():
     # TODO, по идее, так много циклов нам не нужно.
     #  ммы можем идти в цикле по 1 файлу и сразу записывать данные в другой, в случае,
     #  если победитель проходит по количеству балов.
-    player_list = i_line.split()
-    if int(player_list[2]) > points_second_tour:
-        for i_sym, sym in enumerate(player_list[0]):
-            if i_sym ==0:
-                name_string = sym + "."
-        name = name_string + player_list[1]
-        winer_dict[name] = player_list[2]
+    surname_player, name_player, point = i_line.split()
+    if int(point) > points_second_tour:
+        name_string = name_player[0] + "."
+        name = name_string + surname_player
+        winer_dict[name] = point
 
 output_file = open("second_tour.txt", "a")
 # Запись количество побдителей в файл
@@ -28,8 +26,7 @@ output_file.write(player_count)
 for name, points in winer_dict.items():
     # TODO, запись в файл возможно стоит сделать без дополнительной переменной
     #  При помощи форматирования строк =)
-    player_result = name + " " + str(points)+ "\n"
-    output_file.write(player_result)
+    output_file.write(f"{name} {str(points)} \n")
 
 input_file.close()
 output_file.close()
