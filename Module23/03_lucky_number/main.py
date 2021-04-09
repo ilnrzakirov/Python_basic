e058216cb3a55ce3c0863d645642bca4aaa66a9d
Ilnur <ilnrzakirov@gmail.com>
2021-04-09 16:30:34 +0300

    task3

--- Module23/03_lucky_number/main.py
+++ Module23/03_lucky_number/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+
+summ = 0
+count = 0
+
+try:
+    while summ < 777:
+        number = int(input("Введите число: "))
+        count += 1
+        if count == 13:
+            raise ("Случайная ошибка")
+        res_file = open("res.txt", "a")
+        res_file.write("{} \n".format(str(number)))
+        summ += number
+finally:
+    res_file.close()
