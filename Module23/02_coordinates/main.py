a37d26a7c8e777d9a60ce28d41d1b21b97bb41a6
Ilnur <ilnrzakirov@gmail.com>
2021-04-09 16:10:01 +0300

    task2

--- Module23/02_coordinates/main.py
+++ Module23/02_coordinates/main.py
@@ -4,36 +4,38 @@ import random
 def f(x, y):
     x += random.randint(0, 10)
     y += random.randint(0, 5)
-    return x / y
+    try:
+        return x / y
+    except ZeroDivisionError:
+        print("в f: на ноль делить нельзя")
 
 
 def f2(x, y):
     x -= random.randint(0, 10)
     y -= random.randint(0, 5)
-    return y / x
+    try:
+        return y / x
+    except ZeroDivisionError:
+        print("в f2: на ноль делить нельзя")
 
 
 try:
     file = open('coordinates.txt', 'r')
     for line in file:
-        nums_list = line.split()
-        res1 = f(int(nums_list[0]), int(nums_list[1]))
-        try:
-            res2 = f2(int(nums_list[0]), int(nums_list[1]))
-            try:
-                number = random.randint(0, 100)
-                file_2 = open('result.txt', 'w')
-                my_list = sorted([res1, res2, number])
-                file_2.write(' '.join(my_list))
-            except Exception:
-                print("Что-то пошло не так")
-        except Exception:
-            print("Что-то пошло не так со второй функцией")
-        finally:
-            file.close()
-            file_2.close()
-except Exception:
-    print("Что-то пошло не так с первой функцией")
+        num_x, num_y = line.split()
+        res1 = f(int(num_x), int(num_y))
+        res2 = f2(int(num_x), int(num_y))
+        number = random.randint(0, 100)
+        file_2 = open('result.txt', 'a')
+        if file_2.closed:
+            raise ("result.txt закрыт")
+        my_list = sorted([res1, res2, number])
+        file_2.write("{} \n".format(' '.join(str(my_list))))
+except FileNotFoundError:
+    print("Что-то пошло не так")
+finally:
+    file.close()
+    file_2.close()
 
 
 # TODO отредактировать и исправить программу
