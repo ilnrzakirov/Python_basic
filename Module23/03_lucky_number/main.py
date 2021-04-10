dec59a051a6c76044d5c85b4d10d31ee0a3ca3b8
Ilnur <ilnrzakirov@gmail.com>
2021-04-10 11:12:11 +0300

    task2

--- Module23/03_lucky_number/main.py
+++ Module23/03_lucky_number/main.py
@@ -1,14 +1,17 @@
 summ = 0
 count = 0
 
+res_file = open("res.txt", "a")
 try:
     while summ < 777:
         number = int(input("Введите число: "))
         count += 1
         if count == 13:
-            raise ("Случайная ошибка")  # TODO, возможно, лучше добавить Exception
-        res_file = open("res.txt", "a")  # TODO, предлагаю попробовать открывать файл до блока try =)
+            raise Exception # TODO, возможно, лучше добавить
+# TODO, предлагаю попробовать открывать файл до блока try =)
         res_file.write("{} \n".format(str(number)))
         summ += number
+except:
+    print("Случайная ошибка")
 finally:
     res_file.close()
