2b65e40cf70adca66ba257966c2f7f2c1296ab4a
Ilnur <ilnrzakirov@gmail.com>
2021-04-15 19:07:12 +0300

    task2

--- Module23/02_coordinates/main.py
+++ Module23/02_coordinates/main.py
@@ -15,24 +15,27 @@ def f2(x, y):
     return y / x
 
 
-
-try:
-    file = open('coordinates.txt', 'r')
-    for line in file:
+file_2 = open('result.txt', 'a')
+file = open('coordinates.txt', 'r')
+for line in file:
+    try:
         # TODO, стоит ловить ошибки в цикле, иначе, программа завершает свою работу наткнувшись на ошибку в цикле.
         num_x, num_y = line.split()
+        if not str(num_x).isdigit() or not str(num_y).isdigit():
+            raise ValueError
         res1 = f(int(num_x), int(num_y))
         res2 = f2(int(num_x), int(num_y))
         number = random.randint(0, 100)
-        file_2 = open('result.txt', 'a')
         if file_2.closed:
             raise ("result.txt закрыт")
         my_list = sorted([res1, res2, number])
         file_2.write("{} \n".format(' '.join(str(my_list))))
+    except ValueError:
+        print("Входные данные могут быть только целыми числами")
     file.close()
     file_2.close()
-except FileNotFoundError:
-    print("Что-то пошло не так")
+
+
     # TODO, если при открытии файла поймаем ошибку, в таком случае получим ошибку в этом месте.
     #  Возможно лучше создавать/открывать файл тоже до блока try.
 
