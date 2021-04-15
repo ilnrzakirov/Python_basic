5d8521e16e81617fd1e133dff45e8311cffc17bc
Ilnur <ilnrzakirov@gmail.com>
2021-04-15 14:50:57 +0300

    task7

--- Module23/07_text_calc_2/main.py
+++ Module23/07_text_calc_2/main.py
@@ -1,1 +1,49 @@
-# TODO здесь писать код
+
+text_file = open("calc.txt", "a+")
+def is_number (string):
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
+
+count = 0
+text_file.seek(0)
+for i_line in text_file:
+    count +=1
+    try:
+        operand_1, operation ,operand_2 = i_line.split()
+    except ValueError:
+        print("Что то не так с уровнением")
+    all_operation = "+-/%*"
+    if len(operation) > 1:
+        print("Обнаружена ошибка в строке {} {} желаете исправить?: ".format(count, i_line))
+        answer = input().lower()
+        if answer == "да":
+            corrected = input("Введите исправленную версию: ")
+            try:
+                operand_1, operation, operand_2 = corrected.split()
+            except ValueError:
+                print("Что то не так с уровнением")
+        if answer == "нет":
+            continue
+    try:
+        if operation not in all_operation:
+            raise ValueError
+        if is_number(operand_1) == False or is_number(operand_2) == False:
+            raise BaseException
+    except ValueError:
+        print("operation не является символами +, -, *, /, %")
+    except BaseException:
+        print("Использовать можно только цифры")
+    if operation == "+":
+        result = float(operand_1) + float(operand_2)
+    if operation == "-":
+        result = float(operand_1) - float(operand_2)
+    if operation == "*":
+        result = float(operand_1) * float(operand_2)
+    if operation == "/":
+        result = float(operand_1) / float(operand_2)
+    if operation == "%":
+        result = float(operand_1) % float(operand_2)
+    print("Результат: {}".format(result))
