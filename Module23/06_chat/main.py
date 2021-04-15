e7e7433a56400fdb258ba929580520e171b32d4c
Ilnur <ilnrzakirov@gmail.com>
2021-04-15 14:36:40 +0300

    task6

--- Module23/06_chat/main.py
+++ Module23/06_chat/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+
+text_file = open("chat.txt", "a+", encoding="UTF-8")
+
+user = input("Введите имя пользователя: ")
+
+while True:
+    answer = input("Посмотреть текущий чат - 1, отправить сообщение - 2: ")
+    try:
+        if int(answer) == 1:
+            text_file.seek(0)
+            for i_line in text_file.readlines():
+                print(i_line)
+        if int(answer) == 2:
+            message = input("Введите сообщение: ")
+            text_file.write('{}: {} \n'.format(user, message))
+        if int(answer) != 1 and int(answer) != 2:
+            raise ValueError
+    except ValueError:
+        print("Такого варианта собыйтий нет")
+
