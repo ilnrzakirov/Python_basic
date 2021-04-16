6793eb8e4c25c0bdd2ed3f6a5be02202739e09f5
Insolente13 <lanc05@mail.ru>
2021-04-16 15:58:51 +0300

    Module 23 01, 02, 05, 07 - TODO, another - ok

--- Module23/04_registration/main.py
+++ Module23/04_registration/main.py
@@ -2,19 +2,19 @@ def chek(data):
     # , функция должна работать с data, а не с i_line =)
     #  т.к. data это параметр функции, а i_line это внешняя переменная.
 
-        # , предлагаю проверять, если split не состоит из 3х элементов,
-        #  то, выбрасывать ошибку.
+    # , предлагаю проверять, если split не состоит из 3х элементов,
+    #  то, выбрасывать ошибку.
     if len(data.split()) != 3:
-        raise ValueError ("НЕ присутствуют все три поля")
+        raise ValueError("НЕ присутствуют все три поля")
     name, mail, age = data.split()
     if not str(name).isalpha():
-        raise NameError ("поле имени содержит НЕ только буквы")
-            # , таким образом можно вызывать ошибку с определённым текстом NameError("Текст ошибки")
-            #  В таком случае, ошибки можно будет ловить группой.
-    if "." not in mail or "@" not in mail: # , таким образом наличие "@" в mail не проверяем.
-            raise SyntaxError ("поле емейл НЕ содержит @ и .(точку)")
+        raise NameError("поле имени содержит НЕ только буквы")
+        # , таким образом можно вызывать ошибку с определённым текстом NameError("Текст ошибки")
+        #  В таком случае, ошибки можно будет ловить группой.
+    if "." not in mail or "@" not in mail:  # , таким образом наличие "@" в mail не проверяем.
+        raise SyntaxError("поле емейл НЕ содержит @ и .(точку)")
     if 10 > int(age) > 99:
-            raise ValueError ("поле возраст НЕ является числом от 10 до 99")
+        raise ValueError("поле возраст НЕ является числом от 10 до 99")
 
 
 registration_file = open("registrations.txt", "r", encoding="UTF-8")
@@ -28,14 +28,12 @@ for i_line in registration_file:
         chek(i_line)
         registrations_good.write(i_line)
     except (ValueError, NameError, SyntaxError) as err:
-        # TODO, предлагаю добавить синоним "as err" и записывать в файл не только строку, но и тип ошибки.
+        # , предлагаю добавить синоним "as err" и записывать в файл не только строку, но и тип ошибки.
         #  Возможно, стоит добавить символ переноса строки при записи.
         registrations_bad.write("{}:{}".format(err, i_line))
-
-
-
 
 registrations_good.close()
 registrations_bad.close()
 registration_file.close()
 
+# зачёт!
--- Module23/05_text_calc/main.py
+++ Module23/05_text_calc/main.py
@@ -1,9 +1,14 @@
 text_file = open("calc.txt")
 
-def calc (operand_1, operation, operand_2):
-    # TODO Решено правильно, но, давайте объединим условные операторы if в один с помощью блоков elif/else.
+
+def calc(operand_1, operation, operand_2):
+    #  Решено правильно, но, давайте объединим условные операторы if в один с помощью блоков elif/else.
     #  Каждый условный оператор if это новая проверка, которая производится всегда.
     #  В то время, как блоки elif и else могут и не наступить.
+
+    # TODO, для улучшения читаемости кожа, возможно, стоит привести operand_1 и operand_2
+    #  заранее к float, таким образов, у условном операторе float уберём =)
+
     if operation == "+":
         result = float(operand_1) + float(operand_2)
     elif operation == "-":
@@ -15,10 +20,11 @@ def calc (operand_1, operation, operand_2):
     elif operation == "%":
         result = float(operand_1) % float(operand_2)
     return result
-    # TODO, функция должна возвращать результат вычислений.
+    # , функция должна возвращать результат вычислений.
     #  т.к. в итоге, нам необходимо получить сумму всех результатов.
 
-# TODO, лишняя функция. При помощи какого строкового метода можно узнать, является ли строка числом?
+
+# , лишняя функция. При помощи какого строкового метода можно узнать, является ли строка числом?
 def is_number(string):
     try:
         float(string)
@@ -26,16 +32,18 @@ def is_number(string):
     except ValueError:
         return False
 
-def chek_data (i_line):
+
+def chek_data(i_line):
     if len(i_line.split()) != 3:
-        raise ValueError ("Что-то не так с уровнением")
+        raise ValueError("Что-то не так с уровнением")
     operand_1, operation, operand_2 = i_line.split()
     if operation not in all_operation:
-        raise ValueError ("operation не является символами +, -, *, /, %")
-    if not is_number(operand_1) or not is_number(operand_2):  # TODO вместо "== False", лучше использовать "not"
-        raise Exception ("Использовать можно только цифры")
-        # TODO BaseException - внутренняя ошибка python. Уровнем выше есть Exception.
+        raise ValueError("operation не является символами +, -, *, /, %")
+    if not is_number(operand_1) or not is_number(operand_2):  # вместо "== False", лучше использовать "not"
+        raise Exception("Использовать можно только цифры")
+        #  BaseException - внутренняя ошибка python. Уровнем выше есть Exception.
         #  Предлагаю вызывать именно Exception, PyCharm не будет ругаться =)
+
 
 all_operation = "+-/%*"
 for i_line in text_file:
@@ -44,12 +52,11 @@ for i_line in text_file:
         operand_1, operation, operand_2 = i_line.split()
         calc(operand_1, operation, operand_2)
     except (ValueError, Exception):
+        # TODO, предлагаю добавить синоним "as err" и выводить тип ошибки и строку.
         print("Что то не то с уровнением")
 
-
-
-#Z Я не понял как ловить ошибки в основной программе из функций
-# TODO, Предлагаю оптимизировать код в текущей программе, нам потребуется 2 функции.
+# Z Я не понял как ловить ошибки в основной программе из функций
+# , Предлагаю оптимизировать код в текущей программе, нам потребуется 2 функции.
 #  1. для проверки корректности данных и вызова исключения, если данные не корректные.
 #  2. для произведения действий, если данные корректные.
 #  Ловить ошибки необходимо только в основном цикле программы, одним блоком try/except.
