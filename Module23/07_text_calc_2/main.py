be8322f276e77b24f2760b9bc0e3845dc8673aba
Ilnur <ilnrzakirov@gmail.com>
2021-04-16 11:10:22 +0300

    task5

--- Module23/07_text_calc_2/main.py
+++ Module23/07_text_calc_2/main.py
@@ -1,43 +1,6 @@
-text_file = open("calc.txt", "a+")
+text_file = open("calc.txt")
 
-
-def is_number(string):
-    try:
-        float(string)
-        return True
-    except ValueError:
-        return False
-
-
-count = 0
-text_file.seek(0)
-for i_line in text_file:
-    count += 1
-    try:
-        operand_1, operation, operand_2 = i_line.split()
-    except ValueError:
-        print("Что то не так с уровнением")
-    all_operation = "+-/%*"
-    if len(operation) > 1:
-        print("Обнаружена ошибка в строке {} {} желаете исправить?: ".format(count, i_line))
-        answer = input().lower()
-        if answer == "да":
-            corrected = input("Введите исправленную версию: ")
-            try:
-                operand_1, operation, operand_2 = corrected.split()
-            except ValueError:
-                print("Что то не так с уровнением")
-        if answer == "нет":
-            continue
-    try:
-        if operation not in all_operation:
-            raise ValueError
-        if is_number(operand_1) == False or is_number(operand_2) == False:
-            raise BaseException
-    except ValueError:
-        print("operation не является символами +, -, *, /, %")
-    except BaseException:
-        print("Использовать можно только цифры")
+def calc (operand_1, operation, operand_2):
     if operation == "+":
         result = float(operand_1) + float(operand_2)
     if operation == "-":
@@ -50,4 +13,41 @@ for i_line in text_file:
         result = float(operand_1) % float(operand_2)
     print("Результат: {}".format(result))
 
-# TODO, предлагаю оптимизировать код по аналогии с 05 заданием.
+def is_number(string):
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
+
+def chek_data (i_line, count):
+    if len(i_line.split()) != 3:
+        raise ValueError ("Что-то не так с уровнением")
+    operand_1, operation, operand_2 = i_line.split()
+    if operation not in all_operation:
+        raise ValueError ("operation не является символами +, -, *, /, %")
+    if is_number(operand_1) == False or is_number(operand_2) == False:
+        raise BaseException ("Использовать можно только цифры")
+    if len(operation) > 1:
+        print("Обнаружена ошибка в строке {} {} желаете исправить?: ".format(count, i_line))
+        answer = input().lower()
+        if answer == "да":
+            corrected = input("Введите исправленную версию: ")
+            try:
+                operand_1, operation, operand_2 = corrected.split()
+            except ValueError:
+                print("Что то не так с уровнением")
+        if answer == "нет":
+            raise ValueError ("Некорректные данные")
+
+all_operation = "+-/%*"
+count = 0
+for i_line in text_file:
+    count +=1
+    try:
+        chek_data(i_line)
+        operand_1, operation, operand_2 = i_line.split()
+        calc(operand_1, operation, operand_2)
+    except (ValueError, BaseException):
+        print("Что то не то с уровнением")
+
