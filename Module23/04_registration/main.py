ca0b781b73c75470b05d2203148f75981b54f40b
Ilnur <ilnrzakirov@gmail.com>
2021-04-16 10:24:37 +0300

    task4

--- Module23/04_registration/main.py
+++ Module23/04_registration/main.py
@@ -1,34 +1,20 @@
 def chek(data):
     # TODO, функция должна работать с data, а не с i_line =)
     #  т.к. data это параметр функции, а i_line это внешняя переменная.
-    try:
+
         # TODO, предлагаю проверять, если split не состоит из 3х элементов,
         #  то, выбрасывать ошибку.
-        if data.split() != 3:
-            raise ValueError
-        name, mail, age = data.split()
-    except ValueError:
-        registrations_bad.write(data)
-        print("НЕ присутствуют все три поля")
-    try:
-        if not str(name).isalpha():
-            registrations_bad.write(data)
-            raise NameError ("поле имени содержит НЕ только буквы")
+    if len(data.split()) != 3:
+        raise ValueError ("НЕ присутствуют все три поля")
+    name, mail, age = data.split()
+    if not str(name).isalpha():
+        raise NameError ("поле имени содержит НЕ только буквы")
             # TODO, таким образом можно вызывать ошибку с определённым текстом NameError("Текст ошибки")
             #  В таком случае, ошибки можно будет ловить группой.
-        if "." not in mail or "@" not in mail: # TODO, таким образом наличие "@" в mail не проверяем.
-            registrations_bad.write(data)
+    if "." not in mail or "@" not in mail: # TODO, таким образом наличие "@" в mail не проверяем.
             raise SyntaxError ("поле емейл НЕ содержит @ и .(точку)")
-        if 10 > int(age) > 99:
-            registrations_bad.write(data)
+    if 10 > int(age) > 99:
             raise ValueError ("поле возраст НЕ является числом от 10 до 99")
-        registrations_good.write(data)
-    except (NameError, SyntaxError, ValueError):
-        print("Какая то ошибка")
-
-
-
-
 
 
 registration_file = open("registrations.txt", "r", encoding="UTF-8")
@@ -38,7 +24,15 @@ for i_line in registration_file:
     # TODO, ловить ошибки и записывать их в файл предлагаю в основном цикле нашей программы.
     #  В этом месте. Но вызывать внутри функции.
     # Я не понял, как это вызывать внутри функции а ловить здесь? Блок try не нужен? просто raise?
-    chek(i_line)
+    try:
+        chek(i_line)
+        registrations_good.write(i_line)
+    except (ValueError, NameError, SyntaxError):
+        registrations_bad.write(i_line)
+
+
+
+
 registrations_good.close()
 registrations_bad.close()
 registration_file.close()
