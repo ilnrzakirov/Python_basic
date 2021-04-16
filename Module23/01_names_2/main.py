0c7a9bc43ccdbbc9d0cdf9c3e17dd8eb1e0644d9
Insolente13 <lanc05@mail.ru>
2021-04-16 17:13:51 +0300

    Module 23 01, 02, 07 - TODO, another - ok

--- Module23/01_names_2/main.py
+++ Module23/01_names_2/main.py
@@ -3,16 +3,17 @@ line_count = 0
 people_name = open("people.txt", "r", encoding="UTF-8")
 errors = open("errors.log", "a", encoding="UTF-8")
 
- # , создавать/открывать файл для записи ошибок предлагаю до блока try/except.
+# , создавать/открывать файл для записи ошибок предлагаю до блока try/except.
 for i_line in people_name:
-        # TODO, стоит ловить ошибки внутри цикла. Иначе, если в цикле возникает ошибка,
-        #  Из цикла выходим.
+    # , стоит ловить ошибки внутри цикла. Иначе, если в цикле возникает ошибка,
+    #  Из цикла выходим.
     try:
         len_line = len(i_line)
         line_count += 1
         if i_line.endswith("\n"):
             len_line -= 1
         if len_line < 3:
+            # TODO, записывать ошибки в файл предлагаю в блоке except
             error = "Длина {} строки меньше трех символов \n".format(line_count)
             errors.write(error)
             raise Exception  # , возможно, лучше использовать Exception
@@ -22,6 +23,8 @@ for i_line in people_name:
         print("Длина {} строки меньше трех символов".format(line_count))
     finally:
         print("Количество символов: {}".format(total_sym))
+        # TODO, таким образом файл закрывается после первой итерации цикла, но не должен.
+
         people_name.close()
 
-# TODO, если получили ошибку, код не должен прекратить работу.
+# , если получили ошибку, код не должен прекратить работу.
