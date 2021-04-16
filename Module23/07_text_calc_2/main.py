d310350a86f2d5d3e8406225d4ddbebc2313e1ea
Ilnur <ilnrzakirov@gmail.com>
2021-04-16 16:19:38 +0300

    task17

--- Module23/07_text_calc_2/main.py
+++ Module23/07_text_calc_2/main.py
@@ -1,16 +1,18 @@
 text_file = open("calc.txt")
 
 def calc (operand_1, operation, operand_2):
+    operand_1 = float(operand_1)
+    operand_2 = float(operand_2)
     if operation == "+":
-        result = float(operand_1) + float(operand_2)
+        result = operand_1 + operand_2
     if operation == "-":
-        result = float(operand_1) - float(operand_2)
+        result = operand_1 - operand_2
     if operation == "*":
-        result = float(operand_1) * float(operand_2)
+        result = operand_1 * operand_2
     if operation == "/":
-        result = float(operand_1) / float(operand_2)
+        result = operand_1 / operand_2
     if operation == "%":
-        result = float(operand_1) % float(operand_2)
+        result = operand_1 % operand_2
     print("Результат: {}".format(result))
 
 def is_number(string):
@@ -20,14 +22,14 @@ def is_number(string):
     except ValueError:
         return False
 
-def chek_data (i_line, count):
+def chek_data (i_line):
     if len(i_line.split()) != 3:
         raise ValueError ("Что-то не так с уровнением")
     operand_1, operation, operand_2 = i_line.split()
     if operation not in all_operation:
         raise ValueError ("operation не является символами +, -, *, /, %")
     if is_number(operand_1) == False or is_number(operand_2) == False:
-        raise BaseException ("Использовать можно только цифры")
+        raise Exception ("Использовать можно только цифры")
     if len(operation) > 1:
         raise SyntaxError
 
@@ -37,12 +39,12 @@ count = 0
 for i_line in text_file:
     count +=1
     try:
-        chek_data(i_line, count)
+        chek_data(i_line)
         operand_1, operation, operand_2 = i_line.split()
         calc(operand_1, operation, operand_2)
 #    except (ValueError, BaseException):
 #        print("Что то не то с уровнением")
-    except (SyntaxError, ValueError, BaseException):
+    except (SyntaxError, ValueError, Exception):
         print("Обнаружена ошибка в строке {} {} желаете исправить?: ".format(count, i_line))
         answer = input().lower()
         if answer == "да":
