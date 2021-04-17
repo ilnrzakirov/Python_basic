7d25ff8a33a9d2e81ab6ec6ede57d536562d575d
Insolente13 <lanc05@mail.ru>
2021-04-17 20:56:49 +0300

    Module 24 01, 03 - ok, 04, 05, 06, 07, 08, 09 - not yet

--- Module24/01_fight/main.py
+++ Module24/01_fight/main.py
@@ -1,14 +1,16 @@
 import random
 
+
 class Warrior:
 
     def __init__(self, name):
         self.warrior_name = name
         self.health = 100
 
-    def attack (self, object):
+    def attack(self, object):
         if isinstance(object, Warrior):
             object.health -= 20
+
 
 #  у воина должен быть метод.
 #  На вход метод должен принимать второго война и проверять, воин ли он.
@@ -22,7 +24,7 @@ while unit_1.health > 0 and unit_2.health > 0:
     if remains == 1:
         unit_1.attack(unit_2)
         print("Атаковал {}, у противника осталось {} очков жизни".format(unit_1.warrior_name, unit_2.health))
-    if remains == 2:
+    if remains == 2:  # только лучше elif =) Тогда будет одна проверка в некоторых случаях. Сейчас всегда 2
         unit_2.attack(unit_1)
         print("Атаковал {}, у противника осталось {} очков жизни".format(unit_2.warrior_name, unit_1.health))
 
@@ -30,3 +32,5 @@ if unit_1.health == 0:
     print("{} одержал победу".format(unit_2.warrior_name))
 else:
     print("{} одержал победу".format(unit_1.warrior_name))
+
+# зачёт!
--- Module24/03_circle/main.py
+++ Module24/03_circle/main.py
@@ -1,33 +1,37 @@
 import math
 
+
 class Circle:
 
-    def __init__(self, x = 0, y = 0, r = 1):
+    def __init__(self, x=0, y=0, r=1):
         self.x = x
         self.y = y
         self.radius = r
 
-    def square (self):
+    def square(self):
         s = math.pi * self.radius ** 2
         return s
 
-    def perimeter (self):
+    def perimeter(self):
         p = 2 * math.pi * self.radius
         return p
 
-    def zoom (self, k):
+    def zoom(self, k):
         self.radius = self.radius * k
 
-    def checking_intersection (self, object):
+    def checking_intersection(self, object):
         if isinstance(object, Circle):
             suum_radius = self.radius + object.radius
             difference = self.radius - object.radius
             distans = math.sqrt((self.x - object.x) ** 2 + (self.y - object.y) ** 2)
-            if difference < distans < suum_radius:
+            if difference < distans < suum_radius:  # Можно сразу возвращать результат сравнения "return difference < distans < suum_radius"
                 return True
             else:
                 return False
 
+
 circle1 = Circle(0, 0, 1)
 circle2 = Circle(1, 1, 2)
 print(circle1.checking_intersection(circle2))
+
+# зачёт!
