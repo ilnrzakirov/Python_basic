8cbd157ad3a913f00e61dfb02b42685bd37f8b01
Ilnur <ilnrzakirov@gmail.com>
2021-04-29 11:13:07 +0300

    task2

--- Module25/02_karma/main.py
+++ Module25/02_karma/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+import random
+
+def one_day():
+    number = random.randint(1, 10)
+    try:
+        if number == 8:
+            error = random.choice(err)
+            with open("karma.log", "a") as karma_log:
+                karma_log.write(error + "\n")
+            raise error
+    except:
+        print("Ошибка: {}".format(error))
+
+    return random.randint(1, 7)
+
+
+err = ["KillError", "DrunkError", "CarCrashError", "GluttonyError", "DepressionError"]
+karma = 0
+
+while karma < 500:
+    karma += one_day()
+    print("Карма: {}".format(karma))
