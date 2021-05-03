9203e05f5fcd1c9252bdb3928a7aca206a25337f
Ilnur <ilnrzakirov@gmail.com>
2021-05-03 14:39:13 +0300

    task2

--- Module25/02_karma/main.py
+++ Module25/02_karma/main.py
@@ -1,26 +1,39 @@
 import random
 
+class Error:
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
 
 def one_day():
     number = random.randint(1, 10)
     # TODO, эта функция ловить ошибки не должна.
     #  только вызывать. Ловить необходимо внутри основного цикла программы.
-    try:
-        if number == 8:
-            error = random.choice(err)
-            with open("karma.log", "a") as karma_log:
-                karma_log.write(error + "\n")
-            raise error
-    except:  # TODO, давайте вспомним как правильно ловить ошибки =)
-        print("Ошибка: {}".format(error))
 
+    if number == 8:
+        error = random.choice(err)
+        with open("karma.log", "a") as karma_log:
+            karma_log.write(str(error) + "\n")
+        raise error.name
+      # TODO, давайте вспомним как правильно ловить ошибки =)
     return random.randint(1, 7)
 
 
 # TODO, таким образом храним в списке текст. А как хранить классы? =)
-err = ["KillError", "DrunkError", "CarCrashError", "GluttonyError", "DepressionError"]
+KillError = Error("KillError")
+DrunkError = Error("DrunkError")
+CarCrashError = Error("CarCrashError")
+GluttonyError = Error("GluttonyError")
+DepressionError = Error("DepressionError")
+err = [KillError, DrunkError, CarCrashError, GluttonyError, DepressionError]
 karma = 0
 
 while karma < 500:
-    karma += one_day()
-    print("Карма: {}".format(karma))
+    try:
+        karma += one_day()
+        print("Карма: {}".format(karma))
+    except (KillError, DrunkError, CarCrashError, GluttonyError, DepressionError ) as err:
+        print("Ошибка: {}".format(err))
