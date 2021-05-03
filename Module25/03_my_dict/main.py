61fb32a6154a743eb5a661072020eaf37f6f811d
Ilnur <ilnrzakirov@gmail.com>
2021-05-03 17:56:55 +0300

    task3

--- Module25/03_my_dict/main.py
+++ Module25/03_my_dict/main.py
@@ -1,3 +1,20 @@
 
 class MyDict:
 
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+
+    def __str__(self):
+        print(self.key, ":", self.value)
+
+
+    def get(self, key):
+        if key == self.key:
+            return self.value
+        else:
+            return 0
+
+
+num = MyDict("non", 212)
+print(num.get("n2n"))
