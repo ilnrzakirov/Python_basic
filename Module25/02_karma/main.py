4821284a7edae5e66fddcadc59a0fb876ed74724
Ilnur <ilnrzakirov@gmail.com>
2021-05-04 13:31:56 +0300

    task2

--- Module25/02_karma/main.py
+++ Module25/02_karma/main.py
@@ -14,17 +14,14 @@ def one_day():
     #  только вызывать. Ловить необходимо внутри основного цикла программы.
 
     if number == 8:
-        error = random.choice(err)
         # TODO, в этой функции запись в файл не нужна.
         #  Стоит записывать в файл в основном цикле программы
-        with open("karma.log", "a") as karma_log:
-            karma_log.write(str(error) + "\n")
-        raise error.name
+        raise Exception
       # TODO, давайте вспомним как правильно ловить ошибки =)
     return random.randint(1, 7)
 
 
-# TODO, таким образом храним в списке текст. А как хранить классы? =)
+# , таким образом храним в списке текст. А как хранить классы? =)
 KillError = Error("KillError")
 DrunkError = Error("DrunkError")
 CarCrashError = Error("CarCrashError")
@@ -37,5 +34,8 @@ while karma < 500:
     try:
         karma += one_day()
         print("Карма: {}".format(karma))
-    except (KillError, DrunkError, CarCrashError, GluttonyError, DepressionError ) as err:
-        print("Ошибка: {}".format(err))
+    except Exception:
+        error = random.choice(err)
+        with open("karma.log", "a") as karma_log:
+            karma_log.write(str(error) + "\n")
+        print("Ошибка: {}".format(error))
