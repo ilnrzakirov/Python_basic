690f5cc640a44e1e878204dd20f3f28186c4164b
Ilnur <ilnrzakirov@gmail.com>
2021-05-10 15:12:43 +0300

    task2

--- Module25/02_karma/main.py
+++ Module25/02_karma/main.py
@@ -2,44 +2,40 @@ import random
 
 
 class KillError(Exception):
-    name = "KillError"
 
     def __str__(self):
-        return self.name
+        return self.__class__.__name__
 
 
 class DrunkError(Exception):
-    name = "DrunkError"
 
-    # TODO, лишний аргумент =) В таком случае метод str нужен.
+
+    #  лишний аргумент =) В таком случае метод str нужен.
     #  К имени класса можно обратиться через __class__.__name__ =)
 
     def __str__(self):
-        return self.name
+        return self.__class__.__name__
 
 
 class CarCrashError(Exception):
-    name = "CarCrashError"
 
     def __str__(self):
-        return self.name
+        return self.__class__.__name__
 
 
 class GluttonyError(Exception):
-    name = "GluttonyError"
 
     def __str__(self):
-        return self.name
+        return self.__class__.__name__
 
 
 class DepressionError(Exception):
-    name = "DepressionError"
 
     def __str__(self):
-        return self.name
+        return self.__class__.__name__
 
 
-# TODO, предлагаю создавать в этом месте классы KillError, DrunkError, CarCrashError, GluttonyError, DepressionError.
+#  предлагаю создавать в этом месте классы KillError, DrunkError, CarCrashError, GluttonyError, DepressionError.
 #  Которые были бы с родительским классом Exception.
 #  Стоит определить в них текст ошибки, реализовав возврат в методе __str__.
 #  Метод init буде не нужен =)
@@ -60,7 +56,7 @@ def one_day():
 
 # , таким образом храним в списке текст. А как хранить классы? =)
 
-# TODO, если убрать создание объектов классов ниже, то ошибки не будет.
+# , если убрать создание объектов классов ниже, то ошибки не будет.
 #  Получилось, что мы должны хранить классы в списке err, но хранили объекты классов.
 #  Но сами классы при этом существовать перестали, т.к. объекты классов имеют названия классов. %)
 #  Получилось немного запутанным. Выход такой: не использовать для объектов классов названия классов =)
@@ -74,7 +70,7 @@ while karma < 500:
         print("Карма: {}".format(karma))
     # , было бы правильней ловить ошибки в этом месте и их же записывать в файл
 
-    # TODO (KillError, DrunkError) - таким образом ловим 2 ошибки. Предлагаю ловить только те ошибки, что вызываем мы =)
+    #  (KillError, DrunkError) - таким образом ловим 2 ошибки. Предлагаю ловить только те ошибки, что вызываем мы =)
     #  файл с ошибками называется err и одна ошибка называется err.
     #  Что-то одно стоит переименовать.
 
