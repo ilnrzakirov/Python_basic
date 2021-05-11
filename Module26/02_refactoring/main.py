46be5a371c847e8feca730aecb36f9ed5e9f516b
Ilnur <ilnrzakirov@gmail.com>
2021-05-11 13:35:10 +0300

    task2

--- Module26/02_refactoring/main.py
+++ Module26/02_refactoring/main.py
@@ -1,17 +1,30 @@
+
+
 list_1 = [2, 5, 7, 10]
 list_2 = [3, 8, 4, 9]
 to_find = 56
 
-can_continue = True
-for x in list_1:
-    for y in list_2:
-        result = x * y
-        print(x, y, result)
-        if result == to_find:
-            print('Found!!!')
-            can_continue = False
-            break
-    if not can_continue:
-        break
+def gen(list1: list, list2: list, number: int) -> None:
+    for x in list1:
+        for y in list2:
+            yield  x * y
+            if x * y == number:
+                return
+
+result = gen(list_1, list_2, to_find)
+for item in result:
+    print(item)
+
+#can_continue = True
+#for x in list_1:
+#    for y in list_2:
+#        result = x * y
+#        print(x, y, result)
+#        if result == to_find:
+#            print('Found!!!')
+#            can_continue = False
+#            break
+#    if not can_continue:
+#        break
 
 # TODO провести рефакторинг кода
