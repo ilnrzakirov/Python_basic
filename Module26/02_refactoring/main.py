7a3c63aafe456671525ff6b6ee5cb1a12651555c
Insolente13 <lanc05@mail.ru>
2021-05-11 21:39:52 +0300

    Module 26 01 - ok, 02 - TODO, another - not yet

--- Module26/02_refactoring/main.py
+++ Module26/02_refactoring/main.py
@@ -1,22 +1,22 @@
-
-
 list_1 = [2, 5, 7, 10]
 list_2 = [3, 8, 4, 9]
 to_find = 56
 
-def gen(list1: list, list2: list, number: int) -> None:
+
+def gen(list1: list, list2: list, number: int) -> None:  # TODO, вместо None лучше Iterator[str].
     for x in list1:
         for y in list2:
-            yield  x * y
+            yield x * y  # TODO, возврат получился лишний
             if x * y == number:
-                return
+                return  # TODO, в этом месте стоит возвращать результат при помощи yield
+
 
 result = gen(list_1, list_2, to_find)
 for item in result:
     print(item)
 
-#can_continue = True
-#for x in list_1:
+# can_continue = True
+# for x in list_1:
 #    for y in list_2:
 #        result = x * y
 #        print(x, y, result)
