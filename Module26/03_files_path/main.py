5193b9f66793092e506b664eff9c54e969600c20
Ilnur <ilnrzakirov@gmail.com>
2021-05-11 15:58:13 +0300

    task3

--- Module26/03_files_path/main.py
+++ Module26/03_files_path/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+import os
+
+def gen_files_path(find_catalog: str, catalog = os.path.abspath(os.sep)):
+    for path in os.walk(catalog):
+        yield path
+        if find_catalog in path:
+            return
+
+
+
+
+directory = gen_files_path(os.getcwd())
+for item in directory:
+    print(item)
