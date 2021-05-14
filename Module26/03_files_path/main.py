02a4b03ce779613252750725b2bacbddcc7210a4
Ilnur <ilnrzakirov@gmail.com>
2021-05-14 13:37:43 +0300

    task3

--- Module26/03_files_path/main.py
+++ Module26/03_files_path/main.py
@@ -1,14 +1,22 @@
 import os
 
 def gen_files_path(find_catalog: str, catalog = os.path.abspath(os.sep)):
-    for path in os.walk(catalog):
-        yield path
-        if find_catalog in path:
-            return
+    for path in os.listdir(catalog):
+        abs_path = catalog +  "".join(path) + os.sep
+        if os.path.isfile(abs_path):
+            print(abs_path)
+            continue
+        else:
+            if path == find_catalog:
+                print(abs_path)
+                return abs_path
+            else:
+                print(abs_path)
+                try:
+                    gen_files_path(find_catalog, catalog = abs_path)
+                except (PermissionError, NotADirectoryError):
+                    print("Пропускаем")
 
+path_list = ["Users", "Ilnur", "PycharmProjects", "python_basic", "module26"]
 
-
-
-directory = gen_files_path(os.getcwd())
-for item in directory:
-    print(item)
+gen_files_path("04_hof_seq", catalog = os.path.abspath(os.sep) + os.sep.join(path_list) + os.sep )
