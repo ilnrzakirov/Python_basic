c828ca68a4bf774e7c4edd8271601610062eb8ab
Ilnur <ilnrzakirov@gmail.com>
2021-05-14 15:06:23 +0300

    task4

--- Module26/04_hof_seq/main.py
+++ Module26/04_hof_seq/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+from collections.abc import Iterable
+
+
+
+def QHofstadter(qlist = list) -> Iterable [int]:
+    qlist1 = qlist[:]
+    n = 2
+    while True:
+        try:
+            q = qlist1[n - qlist1[n -1]] + qlist1[n - qlist1[n -2]]
+            qlist1.append(q)
+            n += 1
+            yield q
+        except IndexError:
+            return
+
+
+qlist = [1, 1]
+for item in QHofstadter(qlist):
+    print(item)
