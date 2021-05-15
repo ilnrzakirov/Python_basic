3d19bf58dedb8c464543ec5ecd4c00aef8851b4b
Ilnur <ilnrzakirov@gmail.com>
2021-05-15 15:12:36 +0300

    task4

--- Module26/04_hof_seq/main.py
+++ Module26/04_hof_seq/main.py
@@ -7,10 +7,20 @@ from typing import Type
 
 
 
-# TODO, предлагаю попробовать решить задание классов итератором и описать вычисления в методе __next__.
+# , предлагаю попробовать решить задание классов итератором и описать вычисления в методе __next__.
 #  В таком случае, цикл while не потребуется. =)
 class QHof:
-    pass
+
+    def __init__(self):
+        self.Q = [1, 1]
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        q = self.Q[- self.Q[-1]] + self.Q[-self.Q[-2]]
+        self.Q.append(q)
+        return q
 
 def QHofstadter(qlist=list) -> Type["QHofstadter"]:
     qlist1 = qlist[:]
@@ -28,6 +38,9 @@ def QHofstadter(qlist=list) -> Type["QHofstadter"]:
             return
 
 
-qlist = [1, 1]
-for item in QHofstadter(qlist):
-    print(item)
+#qlist = [1, 1]
+#for item in QHofstadter(qlist):
+#    print(item)
+
+
+
