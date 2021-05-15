ddc06e0a9d2e110d84886eb050acda9737f4f2e8
Ilnur <ilnrzakirov@gmail.com>
2021-05-15 19:22:42 +0300

    task4

--- Module26/03_files_path/main.py
+++ Module26/03_files_path/main.py
@@ -7,24 +7,25 @@ def gen_files_path(find_catalog: str, catalog=os.path.abspath(os.sep)):
         #  Предлагаю работать сразу с os.path.join
         abs_path = os.path.join(path) + os.sep
         if os.path.isfile(abs_path):
-            print(abs_path)  # TODO, print лишний, функция должна возвращать элемент.
+            # , print лишний, функция должна возвращать элемент.
             # , если файл, то возвращаем
             yield abs_path
             # , Предлагаю убрать continue, т.к. ничего не пропускает.
-        else:  # TODO, блок else лишний. Стоит проверять, является ли директорией.
+        elif os.path.isdir(abs_path):  # , блок else лишний. Стоит проверять, является ли директорией.
             directory = os.path.basename(abs_path)
             # , если директория, то получаем её базовое название и сравниваем с find_catalog.
             #  Если совпало, в таком случае работу генератора стоит завершить =)
             #  Если не совпало запускаем цикл по нашей функции gen_files_path, т.к. она итерируемая =)
             if directory == find_catalog:
-                print(abs_path) # TODO, print лишний
+                 #, print лишний
                 return abs_path  # , только yield. =)
             else:
                 for path in gen_files_path(abs_path):
-                    # TODO, в этом месте стоит возвращать path при помощи yield
-                    directory = os.path.basename(path)
-                    if directory == find_catalog:
-                        return abs_path
+                    yield path
+                    # , в этом месте стоит возвращать path при помощи yield
+#                    directory = os.path.basename(path)
+#                    if directory == find_catalog:
+#                        return abs_path
 
 
 # , предлагаю запрашивать путь сразу целиком, не разделяя на папки.
