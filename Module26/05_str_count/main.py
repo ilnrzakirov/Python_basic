253dc5acc5447f3347ed6c7cac55dac1d5182c81
Ilnur <ilnrzakirov@gmail.com>
2021-05-17 14:28:31 +0300

    task5

--- Module26/05_str_count/main.py
+++ Module26/05_str_count/main.py
@@ -4,21 +4,26 @@ import os
 def code_counter(path):
     result = 0
     directory = path  # директория для подсчета
-    for path in os.listdir(directory):
-        new_path = directory + "".join(path)
+    for ipath in os.listdir(directory):
+        new_path = directory + "".join(ipath)
         if str(new_path).endswith(".py"):
-            with open(str(path), "r") as file:
-                 # , глобальная переменная получилась лишней.
-                for line in file:
-                    # , возможно, стоит просто идти по строке, проверяя, что первый символ не "#"
-                    #  split возможно лишний.
-                    if line:
-                        if not line.startswith("#"):
-                            result += 1
+            result += code_counter_next(ipath)
+    return result
+
+def code_counter_next(path):
+    result = 0
+    with open(str(path), "r") as file:
+        # , глобальная переменная получилась лишней.
+        for line in file:
+            # , возможно, стоит просто идти по строке, проверяя, что первый символ не "#"
+            #  split возможно лишний.
+            if line:
+                if not line.startswith("#"):
+                    result += 1
     return result
 
 
-# TODO, директорию необходимо запросить у пользователя и передать в функцию.
+# , директорию необходимо запросить у пользователя и передать в функцию.
 #  Максимальный уровень вложенности кода не должен превышать 3. Возможно, стоит разбить нашу функцию на несколько функций. =)
 
 # path = input("Введите директорию для подсчета: ")
