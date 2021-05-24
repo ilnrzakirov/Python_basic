a5b9f8b18adac6b50fa24d75ba7e9c524f44f0d1
Insolente13 <lanc05@mail.ru>
2021-05-24 23:45:53 +0300

    Module 26 01, 02, 04, 05 - ok, 07 - not yet, another - TODO

--- Module26/03_files_path/main.py
+++ Module26/03_files_path/main.py
@@ -1,5 +1,6 @@
 import os
 
+
 def gen_files_path(find_catalog: str, catalog=os.path.abspath(os.sep)):
     for path in os.listdir(catalog):
         # , возможно, конкатенация и join получились лишними.
@@ -16,20 +17,26 @@ def gen_files_path(find_catalog: str, catalog=os.path.abspath(os.sep)):
             #  Если совпало, в таком случае работу генератора стоит завершить =)
             #  Если не совпало запускаем цикл по нашей функции gen_files_path, т.к. она итерируемая =)
             if directory == find_catalog:
-                 #, print лишний
+                # , print лишний
                 return abs_path  # , только yield. =)
             else:
                 for path in gen_files_path(abs_path):
                     yield path
                     # , в этом месте стоит возвращать path при помощи yield
+
+
 #                    directory = os.path.basename(path)
 #                    if directory == find_catalog:
 #                        return abs_path
 
 
 # , предлагаю запрашивать путь сразу целиком, не разделяя на папки.
-#path_list = ["Users", "Ilnur", "PycharmProjects", "python_basic", "module26"]
+# path_list = ["Users", "Ilnur", "PycharmProjects", "python_basic", "module26"]
 path_folder = input("Введите путь для поиска: ")
 find = input("Введите папку для поиска")
-#gen_files_path("04_hof_seq", catalog=os.path.abspath(os.sep) + os.sep.join(path_list) + os.sep)
+# gen_files_path("04_hof_seq", catalog=os.path.abspath(os.sep) + os.sep.join(path_list) + os.sep)
 gen_files_path(find, path_folder)
+
+# TODO Введите путь для поиска: C:\...\ilnur_zakirov\python_basic
+#  Введите папку для поиска 03_files_path
+#  Пока что программа не выводит ничего =) Предлагаю добавить вывод возвратов Генератора и проверить.
