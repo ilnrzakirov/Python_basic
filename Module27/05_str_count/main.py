dfd079cc30e0731a1ad1ca5ec017efb8f1c32170
Ilnur <ilnrzakirov@gmail.com>
2021-05-28 16:10:30 +0300

    task5

--- Module27/05_str_count/main.py
+++ Module27/05_str_count/main.py
@@ -1,1 +1,34 @@
-# TODO здесь писать код
+from typing import Any, Callable
+
+import functools
+
+def counter(func: Callable)-> Callable:
+    """
+    Декартор считающий количество вызывов функции
+    :param func: Callable
+    :return: Callable
+    """
+    functools.wraps(func)
+    func.count = 0
+    def wrapper(*args, **kwargs)-> Any:
+        func.count += 1
+        print(f"Фунцкия вызывалась {func.count} раз")
+        return func(*args, **kwargs)
+    return wrapper
+
+
+@counter
+def square(num: int)-> int:
+    """
+    Функция возвращающая квадрат числа
+    :param num: int
+    :return: int
+    """
+    return num ** 2
+
+square(2)
+square(2)
+square(2)
+square(2)
+square(2)
+square(2)
