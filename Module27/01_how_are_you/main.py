bac64fb427b4f35c28c2fb3b486ee4be1f9742dd
Ilnur <ilnrzakirov@gmail.com>
2021-05-28 15:03:22 +0300

    task1

--- Module27/01_how_are_you/main.py
+++ Module27/01_how_are_you/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+from typing import Callable, Any
+
+
+def how_are_you(func: Callable)-> Callable:
+    """
+    Декаратор спрашивающий самочувствие пользователя =)
+    :return: Callable
+    """
+    def wrapper()-> Any:
+        input("Как дела: ")
+        print("А у меня не очень! Ладно, держи свою функцию")
+        return func()
+    return wrapper
+
+
+@how_are_you
+def test():
+    print('<Тут что-то происходит...>')
+
+
+test()
