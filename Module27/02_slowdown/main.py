6792874436bcb178e80de8601f45f5568f4d279a
Ilnur <ilnrzakirov@gmail.com>
2021-05-28 15:11:23 +0300

    task2

--- Module27/02_slowdown/main.py
+++ Module27/02_slowdown/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+import time
+from typing import Callable, Any
+from time import sleep
+
+
+def how_are_you(func: Callable)-> Callable:
+    """
+    Декаратор спрашивающий самочувствие пользователя =)
+    :return: Callable
+    """
+    def wrapper()-> Any:
+        time.sleep(5)
+        return func()
+    return wrapper
+
+
+@how_are_you
+def test():
+    print('<Тут что-то происходит...>')
+
+
+test()
