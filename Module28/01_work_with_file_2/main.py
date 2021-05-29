10a9a5313069e9cf26951b8d8d7694f08d42eb69
Insolente13 <lanc05@mail.ru>
2021-05-29 15:59:36 +0300

    Module 28 01, 04 - TODO, another - not yet

--- Module28/01_work_with_file_2/main.py
+++ Module28/01_work_with_file_2/main.py
@@ -1,14 +1,21 @@
 
 
 class Manager_file:
+    # TODO, стоит запрашивать дополнительно "режим" для открытия файла.
     def __init__(self, path: str)-> None:
         self.name = path
 
     def __enter__(self):
+        # TODO, предлагаю попробовать ловить ошибки.
+        #  К примеру, если пользователь некорректно указал "режим", то пытаться открыть файл насильно любым способом.
+        #  Например, при помощи "w".
         self.file = open(self.name, "w")
         return self.file
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        # TODO, стоит ловить ошибки.
+        #  Если поймали, то пробуем закрыть файл и возвращаем Истину =)
+
         if self.file:
             self.file.close()
         return True
--- Module28/04_date/main.py
+++ Module28/04_date/main.py
@@ -2,8 +2,11 @@ import datetime
 
 
 class Date:
+    # TODO, предлагаю сделать 3 параметра. День, месяц и год, вместо одного =)
     def __init__(self, date_string: str):
         self.date_string = date_string
+
+    # TODO, стоит добавить метод __str__ и возвращать дату в удобном формате =)
 
     @classmethod
     def is_date_valid(cls, date_string: str)-> bool:
@@ -17,6 +20,8 @@ class Date:
     def date_from_string(cls, date_string: str)->datetime:
         res = datetime.datetime.strptime(date_string, "%d-%m-%Y")
         print(res)
+        # TODO, стоит добавить возврат себя с нужными данными, которые мы передаём в init. =)
+        #  Если поправить init, то нужно будет разбить дату на 3 параметра и предавать их при возврате себя.
 
 
 date = Date.date_from_string(date_string="10-12-2016")
