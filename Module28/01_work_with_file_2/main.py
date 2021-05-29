f2e58630422667238548d683e77ed863dfc541d6
Ilnur <ilnrzakirov@gmail.com>
2021-05-29 15:14:51 +0300

    task1

--- Module28/01_work_with_file_2/main.py
+++ Module28/01_work_with_file_2/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+
+
+class Manager_file:
+    def __init__(self, path: str)-> None:
+        self.name = path
+
+    def __enter__(self):
+        self.file = open(self.name, "w")
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.file:
+            self.file.close()
+        return True
+
+
+with Manager_file("text.txt") as file:
+    file.write("Hello ")
