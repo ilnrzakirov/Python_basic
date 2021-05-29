63a745582cf8e8d6f6c820ecae1e814a67df134d
Ilnur <ilnrzakirov@gmail.com>
2021-05-29 16:20:27 +0300

    task4

--- Module28/04_date/main.py
+++ Module28/04_date/main.py
@@ -2,11 +2,16 @@ import datetime
 
 
 class Date:
-    # TODO, предлагаю сделать 3 параметра. День, месяц и год, вместо одного =)
-    def __init__(self, date_string: str):
-        self.date_string = date_string
+    # , предлагаю сделать 3 параметра. День, месяц и год, вместо одного =)
+    def __init__(self, year: datetime, mont: datetime, day: datetime):
+        self.year = year
+        self.mont = mont
+        self.day = day
 
-    # TODO, стоит добавить метод __str__ и возвращать дату в удобном формате =)
+
+    # , стоит добавить метод __str__ и возвращать дату в удобном формате =)
+    def __str__(self)-> str:
+        return  (f"Год: {self.year}, Месяц: {self.mont}, День: {self.day}")
 
     @classmethod
     def is_date_valid(cls, date_string: str)-> bool:
@@ -19,11 +24,15 @@ class Date:
     @classmethod
     def date_from_string(cls, date_string: str)->datetime:
         res = datetime.datetime.strptime(date_string, "%d-%m-%Y")
-        print(res)
-        # TODO, стоит добавить возврат себя с нужными данными, которые мы передаём в init. =)
+        year = res.year
+        month = res.month
+        day = res.day
+        return Date(year=year, mont=month, day=day)
+        # , стоит добавить возврат себя с нужными данными, которые мы передаём в init. =)
         #  Если поправить init, то нужно будет разбить дату на 3 параметра и предавать их при возврате себя.
 
 
 date = Date.date_from_string(date_string="10-12-2016")
+print(date)
 print(Date.is_date_valid('10-12-2077'))
 print(Date.is_date_valid('40-12-2077'))
