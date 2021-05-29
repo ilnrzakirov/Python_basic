7ac6e680e4bedfd4666e274235135d65f5ac12fc
Ilnur <ilnrzakirov@gmail.com>
2021-05-29 16:48:11 +0300

    task1

--- Module28/01_work_with_file_2/main.py
+++ Module28/01_work_with_file_2/main.py
@@ -1,25 +1,29 @@
 
 
 class Manager_file:
-    # TODO, стоит запрашивать дополнительно "режим" для открытия файла.
-    def __init__(self, path: str)-> None:
+    # , стоит запрашивать дополнительно "режим" для открытия файла.
+    def __init__(self, path: str, metod: str)-> None:
         self.name = path
+        self.metod = metod
 
     def __enter__(self):
-        # TODO, предлагаю попробовать ловить ошибки.
+        # , предлагаю попробовать ловить ошибки.
         #  К примеру, если пользователь некорректно указал "режим", то пытаться открыть файл насильно любым способом.
         #  Например, при помощи "w".
-        self.file = open(self.name, "w")
+        try:
+            self.file = open(self.name, self.metod)
+        except:
+            self.file = open(self.name, "w")
         return self.file
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         # TODO, стоит ловить ошибки.
         #  Если поймали, то пробуем закрыть файл и возвращаем Истину =)
-
-        if self.file:
-            self.file.close()
+        if exc_type:
+            print(exc_type)
+        self.file.close()
         return True
 
 
-with Manager_file("text.txt") as file:
-    file.write("Hello ")
+with Manager_file("text.txt", "w") as file:
+    file.eror("Hello ")
