be267a8e78f3964b307747086d93eff9c96d6da7
Ilnur <ilnrzakirov@gmail.com>
2021-05-29 15:52:08 +0300

    task4

--- Module28/02_math_module/main.py
+++ Module28/02_math_module/main.py
@@ -1,1 +1,5 @@
-# TODO здесь писать код
+from abc import ABC, abstractmethod
+
+class MyMath(ABC):
+    def __init__(self, ):
+        pass
--- Module28/04_date/main.py
+++ Module28/04_date/main.py
@@ -1,1 +1,24 @@
-# TODO здесь писать код
+import datetime
+
+
+class Date:
+    def __init__(self, date_string: str):
+        self.date_string = date_string
+
+    @classmethod
+    def is_date_valid(cls, date_string: str)-> bool:
+        try:
+            if datetime.datetime.strptime(date_string, "%d-%m-%Y"):
+                return True
+        except (ValueError):
+            return False
+
+    @classmethod
+    def date_from_string(cls, date_string: str)->datetime:
+        res = datetime.datetime.strptime(date_string, "%d-%m-%Y")
+        print(res)
+
+
+date = Date.date_from_string(date_string="10-12-2016")
+print(Date.is_date_valid('10-12-2077'))
+print(Date.is_date_valid('40-12-2077'))
