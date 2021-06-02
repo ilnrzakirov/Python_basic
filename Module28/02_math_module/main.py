f866eca0561172157df9bbb2161a39124976839c
Ilnur <ilnrzakirov@gmail.com>
2021-06-02 22:31:07 +0300

    task2

--- Module28/02_math_module/main.py
+++ Module28/02_math_module/main.py
@@ -1,5 +1,25 @@
 from abc import ABC, abstractmethod
+import math
 
 class MyMath(ABC):
-    def __init__(self, ):
-        pass
+    @classmethod
+    def len_circle(cls, r: float)-> float:
+        return 2 * math.pi * r
+
+    @classmethod
+    def square_circle(cls, r: float)-> float:
+        return math.pi * r ** 2
+
+    @classmethod
+    def volume_cube(cls, length: float)-> float:
+        return length ** 3
+
+    @classmethod
+    def surface_area(cls, r: float)-> float:
+        return 4 * math.pi * r
+
+
+res1 = MyMath.len_circle(r=5)
+res2 = MyMath.square_circle(r=6)
+print(res1)
+print(res2)
--- Module28/03_modeling/main.py
+++ Module28/03_modeling/main.py
@@ -2,29 +2,73 @@ import math
 
 
 class Square:
-    def __init__(self, length: int):
+    def __init__(self, length: float):
         self.length = length
 
-    def perimeter(self):
+    def perimeter(self) -> float:
         p = 4 * self.length
         return p
 
-    def square_area(self):
+    def square(self) -> float:
         s = self.length ** 2
+        return s
+
+#    @property
+#    def length(self) -> float:
+#        return self.length
+
+#    @length.setter
+#    def length(self, length: float) -> None:
+#        self.length = length
+
+
+class FigureSquareMixin:
+    @classmethod
+    def square_figure(cls, figure_list: list) -> float:
+        s = 0
+        for figure in figure_list:
+            s += figure.square()
+        return s
 
 
 class Triangle:
-    def __init__(self, lenght: int, h: int):
-        self.lenght = lenght
+    def __init__(self, length: float, h: float):
+        self.length = length
         self.h = h
 
-    def perimeter(self):
-        a = math.sqrt((1 * self.lenght ** 2) + self.h ** 2)
-        p = self.lenght + a ** 2
+    def perimeter(self) -> float:
+        a = math.sqrt((1 * self.length ** 2) + self.h ** 2)
+        p = self.length + a ** 2
         return p
 
-    def square(self):
-        s = 0.5 * self.lenght * self.h
+    def square(self) -> float:
+        s = 0.5 * self.length * self.h
         return s
 
-# TODO, предлагаю для расчёта площади каждой фигуры использовать класс Mixin =)
+#    @property
+#    def length(self):
+#        return self.length
+
+#    @length.setter
+#    def length(self, length)-> None:
+#        self.length = length
+
+
+class Pyramid(Triangle, Square, FigureSquareMixin):
+    # , предлагаю для расчёта площади каждой фигуры использовать класс Mixin =)
+    def __init__(self, length: float, h: float):
+        super().__init__(length, h)
+        self.element_pyramid = [Triangle(length, h) for _ in range(4)]
+        self.element_pyramid.append(Square(length))
+
+
+class Cube(Square, FigureSquareMixin):
+    def __init__(self, length: float):
+        super().__init__(length)
+        self.element_cube = [Square(length) for _ in range(6)]
+
+
+p = Pyramid(5,5)
+print(p.square_figure(p.element_pyramid))
+c = Cube(5)
+print(c.square_figure(c.element_cube))
