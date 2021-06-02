546cc483ba784041683e88925be21749aba63b7a
Ilnur <ilnrzakirov@gmail.com>
2021-06-02 22:35:44 +0300

    task2

--- Module28/02_math_module/main.py
+++ Module28/02_math_module/main.py
@@ -2,20 +2,43 @@ from abc import ABC, abstractmethod
 import math
 
 class MyMath(ABC):
+    """
+    Абстрактный класс
+    """
     @classmethod
     def len_circle(cls, r: float)-> float:
+        """
+        Метод для нахождения длины окружности
+        :param r: float
+        :return: float
+        """
         return 2 * math.pi * r
 
     @classmethod
     def square_circle(cls, r: float)-> float:
+        """
+        Метод для нахождения площади круга
+        :param r: float
+        :return: float
+        """
         return math.pi * r ** 2
 
     @classmethod
     def volume_cube(cls, length: float)-> float:
+        """
+        Метод для нахождения объема куба
+        :param length: float
+        :return: float
+        """
         return length ** 3
 
     @classmethod
     def surface_area(cls, r: float)-> float:
+        """
+        Метод для нахождения площади поверхности сферы
+        :param r: float
+        :return: float
+        """
         return 4 * math.pi * r
 
 
