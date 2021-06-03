9e68dedaf82b5f4f60aad76302285fce28dd74d3
Ilnur <ilnrzakirov@gmail.com>
2021-06-03 10:38:13 +0300

    task3

--- Module28/03_modeling/main.py
+++ Module28/03_modeling/main.py
@@ -1,25 +1,26 @@
 import math
+from typing import Any
 
 
 class Square:
     def __init__(self, length: float):
-        self.length = length
+        self._length = length
 
     def perimeter(self) -> float:
-        p = 4 * self.length
+        p = 4 * self._length
         return p
 
     def square(self) -> float:
-        s = self.length ** 2
+        s = self._length ** 2
         return s
 
-#    @property
-#    def length(self) -> float:
-#        return self.length
+    @property
+    def length(self) -> float:
+        return self.length
 
-#    @length.setter
-#    def length(self, length: float) -> None:
-#        self.length = length
+    @length.setter
+    def length(self, length: float) -> None:
+        self.length = length
 
 
 class FigureSquareMixin:
@@ -33,35 +34,35 @@ class FigureSquareMixin:
 
 class Triangle:
     def __init__(self, length: float, h: float):
-        self.length = length
+        self._length = length
         self.h = h
 
     def perimeter(self) -> float:
-        a = math.sqrt((1 * self.length ** 2) + self.h ** 2)
-        p = self.length + a ** 2
+        a = math.sqrt((1 * self._length ** 2) + self.h ** 2)
+        p = self._length + a ** 2
         return p
 
     def square(self) -> float:
-        s = 0.5 * self.length * self.h
+        s = 0.5 * self._length * self.h
         return s
 
 
     # TODO таким образом и метод класса и аргумент имеют одинаковое название => length.
     #  Стоит добавить к названию аргумента "_" =)
-#    @property
-#    def length(self):
-#        return self.length
+    @property
+    def length(self):
+        return self.length
 
-#    @length.setter
-#    def length(self, length)-> None:
-#        self.length = length
+    @length.setter
+    def length(self, length)-> None:
+        self.length = length
 
 
 class Pyramid(Triangle, Square, FigureSquareMixin):
     # , предлагаю для расчёта площади каждой фигуры использовать класс Mixin =)
     def __init__(self, length: float, h: float):
         super().__init__(length, h)
-        self.element_pyramid = [Triangle(length, h) for _ in range(4)]
+        self.element_pyramid: Any = [Triangle(length, h) for _ in range(4)]
         self.element_pyramid.append(Square(length))
 
 
