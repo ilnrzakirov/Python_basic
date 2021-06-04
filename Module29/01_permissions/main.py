0a6df60cc105d825a428fe68879257d99fe6f955
Ilnur <ilnrzakirov@gmail.com>
2021-06-04 14:00:19 +0300

    task1

--- Module29/01_permissions/main.py
+++ Module29/01_permissions/main.py
@@ -1,1 +1,32 @@
-# TODO здесь писать код
+import functools
+from collections.abc import Callable
+
+
+def check_permission(user: str) -> Callable:
+    def decarator(func: Callable) -> Callable:
+
+        @functools.wraps(func)
+        def wrapper():
+            if user == "admin":
+                func()
+            else:
+                print(f"У пользователя {user} недостаточно прав, что бы вызвать функцию {func.__name__}  ")
+        return wrapper
+    return decarator
+
+
+user_permissions = ['admin']
+
+
+@check_permission('admin')
+def delete_site():
+    print('Удаляем сайт')
+
+
+@check_permission('user_1')
+def add_comment():
+    print('Добавляем комментарий')
+
+
+delete_site()
+add_comment()
