d001d48c6ab621f7e9879cbd4f72ad98da5498d8
Ilnur <ilnrzakirov@gmail.com>
2021-06-04 14:45:55 +0300

    task2

--- Module29/02_callback/main.py
+++ Module29/02_callback/main.py
@@ -1,1 +1,30 @@
-# TODO здесь писать код
+from collections.abc import Callable
+import functools
+from flask import Flask
+
+app = Flask(__name__)
+
+
+def callback(command: str) -> Callable:
+    def decorate(func: Callable) -> Callable:
+
+        @functools.wraps(func)
+        def wrapper():
+            if command == "//":
+                example()
+        return wrapper
+    return decorate
+
+
+@callback('//')
+def example():
+    print('Пример функции, которая возвращает ответ сервера')
+    return 'OK'
+
+
+route = app.get('//')
+if route:
+    response = route(example)
+    print('Ответ:', response)
+else:
+    print('Такого пути нет')
