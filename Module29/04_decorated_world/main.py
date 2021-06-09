7390b2f7796d31a2bd9208f53324747678bf8dc0
Ilnur <ilnrzakirov@gmail.com>
2021-06-09 17:34:56 +0300

    task4

--- Module29/04_decorated_world/main.py
+++ Module29/04_decorated_world/main.py
@@ -1,26 +1,23 @@
 from collections.abc import Callable
 import functools
 
-# TODO, пожалуйста, обратите внимание, арги и кварги в этом месте получились лишними.
+# , пожалуйста, обратите внимание, арги и кварги в этом месте получились лишними.
 #  Стоит добавить ещё одну "обёртку" для аргов и кваргов. А в эту функцию передавать декоратор
-def decorator_with_args_for_any_decorator(func: Callable) -> Callable:
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        print(f"Приведенные арги и кварки в декоратор: {args}, {kwargs}")
-        result = func(*args, **kwargs)
-        return result
-    return wrapper
+def decorator_with_args_for_any_decorator(decorator: Callable) -> Callable:
+    def decorator_arg(*args, **kwargs):
+        def wrapper(func: Callable):
+            print(f"Приведенные арги и кварки в декоратор: {args}, {kwargs}")
+            return decorator(func, *args, **kwargs)
+        return wrapper
+    return decorator_arg
 
 
 @decorator_with_args_for_any_decorator
-def decorated_decorator(_func: Callable, *args, **kwargs):
-    def decorate(func, *args, **kwargs):
+def decorated_decorator(func: Callable, *args, **kwargs):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            result = func(*args, **kwargs)
-            return result
+            return func(*args, **kwargs)
         return wrapper
-    return decorate
 
 
 
