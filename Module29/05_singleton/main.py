bd4a4a2b6b4ef3df9201dc4100e2463b3fb15e63
Ilnur <ilnrzakirov@gmail.com>
2021-06-10 14:44:55 +0300

    task5

--- Module29/05_singleton/main.py
+++ Module29/05_singleton/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+import functools
+
+def singleton(cls):
+    @functools.wraps(cls)
+    def wrapper():
+        if hasattr(cls, "init") is False:
+            cls.init = cls.__new__
+        return cls.init
+    return wrapper
+
+
+@singleton
+class Example:
+    pass
+
+
+my_obj = Example()
+my_another_obj = Example()
+
+print(id(my_obj))
+print(id(my_another_obj))
+
+print(my_obj is my_another_obj)
