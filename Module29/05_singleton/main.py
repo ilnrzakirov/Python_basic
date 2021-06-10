04fc49078449b7a9cb2a0830903791cc52699253
Insolente13 <lanc05@mail.ru>
2021-06-10 23:01:45 +0300

    Module 29 01, 02, 03, 04 - ok, 05 - TODO

--- Module29/05_singleton/main.py
+++ Module29/05_singleton/main.py
@@ -2,11 +2,18 @@ import functools
 
 def singleton(cls):
     @functools.wraps(cls)
+
     def wrapper():
+
         if hasattr(cls, "init") is False:
             cls.init = cls.__new__
         return cls.init
     return wrapper
+
+# TODO, Возможно, не очень правильная идея опираться на класс init.
+#  Если учесть, что всё в python является объектами класса, возможно будет лучше создать аргумент у функции декоратора
+#  и присвоить значение объекта класса ему. Помните, как мы создавали аргумент для подсчёта количества вызовов функций
+#  в предыдущем уроке?
 
 
 @singleton
