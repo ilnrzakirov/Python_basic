37e5d628de93afc4212131906239f5d418862b9a
Ilnur <ilnrzakirov@gmail.com>
2021-06-11 16:00:29 +0300

    task2

--- Module30/02_timeit/main.py
+++ Module30/02_timeit/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+import timeit
+
+
+res: float = timeit.timeit('"-".join(str(n) for n in range(100))', number=10000)
+print(res)
+
+res2: float = timeit.timeit('"-".join([str(n) for n in range (100)])', number=10000)
+print(res2)
+
+res3: float = timeit.timeit('"-".join(map(str, range(100)))', number=10000)
+print(res3)
