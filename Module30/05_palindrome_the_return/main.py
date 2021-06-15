0355dde1f8cac61172c5c7cddf3149269e1ebbec
Ilnur <ilnrzakirov@gmail.com>
2021-06-15 15:15:42 +0300

    task5

--- Module30/05_palindrome_the_return/main.py
+++ Module30/05_palindrome_the_return/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+import collections
+
+def can_be_poly(string: str) -> bool:
+    sym_list = collections.Counter(string)
+    one_letter = 0
+    for sym in sym_list:
+        if sym_list[sym] % 2 == 0:
+            continue
+        else:
+            one_letter += 1
+    if one_letter == 1 or one_letter == 0:
+        return True
+    else:
+        return False
+
+
+print(can_be_poly('ababc'))
+print(can_be_poly('abbbc'))
