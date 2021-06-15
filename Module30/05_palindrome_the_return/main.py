e75ab412b991498cbd3596b14c35453a32b76268
Ilnur <ilnrzakirov@gmail.com>
2021-06-15 17:03:16 +0300

    task5

--- Module30/05_palindrome_the_return/main.py
+++ Module30/05_palindrome_the_return/main.py
@@ -2,17 +2,21 @@ import collections
 
 
 def can_be_poly(string: str) -> bool:
-    sym_list = collections.Counter(string)
-    one_letter = 0
-    for sym in sym_list:
-        if sym_list[sym] % 2 == 0:
-            continue
-        else:
-            one_letter += 1
-    if one_letter == 1 or one_letter == 0:
-        return True
-    else:
+    res = list(filter(lambda count: count % 2 != 0 , collections.Counter(string).values()))
+    if len(res) > 1:
         return False
+    else:
+        return True
+#    one_letter = 0
+#    for sym in sym_list:
+#        if sym_list[sym] % 2 == 0:
+#            continue
+#        else:
+#            one_letter += 1
+#    if one_letter == 1 or one_letter == 0:
+#        return True
+#    else:
+#        return False
 
 
 print(can_be_poly('ababc'))
