e149adc3b3cdf35463f5c12eefdcb9e2d29f4e22
Ilnur <ilnrzakirov@gmail.com>
2021-06-15 15:02:26 +0300

    task4

--- Module30/04_code_readability/main.py
+++ Module30/04_code_readability/main.py
@@ -1,3 +1,11 @@
 
-res = []
-print(list(res))
+res = [x for x in range (2, 1000) if not [y for y in range(2, x) if not x%y]]
+res1 = []
+for number in range(2, 1000):
+    for x in range(2, number):
+        if number % x == 0:
+            break
+    else:
+        res1.append(number)
+print(res)
+print(res1)
