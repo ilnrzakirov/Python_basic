78e922f931530cddbb889171335e0e8472b012df
Insolente13 <lanc05@mail.ru>
2021-07-18 20:26:33 +0300

    Module 31 01, 02 - ok, 03 - TODO

--- Module31/03_breaking_bad/main.py
+++ Module31/03_breaking_bad/main.py
@@ -4,6 +4,7 @@ import json
 
 data_on_deaths = requests.get('https://breakingbadapi.com/api/deaths').text
 data_episode = requests.get('https://breakingbadapi.com/api/episodes').text
+# TODO, к data_on_deaths стоит применить json.loads (десериализация объекта), и только после этого передать в функцию max.
 
 max_deaths = max(data_on_deaths, key=lambda x: x['number_of_deaths'])
 print(max_deaths)
