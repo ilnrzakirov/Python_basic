fba150c7481b9c60287465dc94ad45b67c4bc8cf
Ilnur <ilnrzakirov@gmail.com>
2021-07-18 19:03:48 +0300

    task2

--- Module31/03_breaking_bad/main.py
+++ Module31/03_breaking_bad/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+import requests
+import json
+
+
+data_on_deaths = requests.get('https://breakingbadapi.com/api/deaths').text
+data_episode = requests.get('https://breakingbadapi.com/api/episodes').text
+
+max_deaths = max(data_on_deaths, key=lambda x: x['number_of_deaths'])
+print(max_deaths)
+
