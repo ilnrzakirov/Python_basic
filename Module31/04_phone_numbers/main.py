35d62cb88f2c18d30edd0fb2e46bb646a7d64d6e
Ilnur <ilnrzakirov@gmail.com>
2021-07-19 13:25:31 +0300

    task4

--- Module31/04_phone_numbers/main.py
+++ Module31/04_phone_numbers/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+import re
+
+number = ['9999999999', '999999-999', '99999x9999']
+i = 0
+
+pettern = r'\b[89]\d{9}'
+
+while i < len(number):
+    if re.findall(pettern, number[i]):
+        print(number[i], ': все в порядке')
+    else:
+        print(number[i], ": не подходит")
+    i = i + 1
