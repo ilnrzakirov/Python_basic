20c883735e6d3cb69ec49357b6777ac2802d94c9
Ilnur <ilnrzakirov@gmail.com>
2021-07-19 13:06:42 +0300

    task3

--- Module31/03_breaking_bad/main.py
+++ Module31/03_breaking_bad/main.py
@@ -1,11 +1,25 @@
 import requests
 import json
 
+temp = dict()
 
-data_on_deaths = requests.get('https://breakingbadapi.com/api/deaths').text
-data_episode = requests.get('https://breakingbadapi.com/api/episodes').text
+data_on_deaths = json.loads(requests.get('https://breakingbadapi.com/api/deaths').text)
+data_episode = json.loads(requests.get('https://breakingbadapi.com/api/episodes').text)
 # TODO, к data_on_deaths стоит применить json.loads (десериализация объекта), и только после этого передать в функцию max.
 
 max_deaths = max(data_on_deaths, key=lambda x: x['number_of_deaths'])
-print(max_deaths)
 
+for episode in data_episode:
+    if int(episode['episode']) == int(max_deaths['episode']):
+        temp = episode
+        break
+
+result = dict()
+result['season'] = temp['season']
+result['episode'] = temp['episode']
+result['number_of_deaths'] = max_deaths['number_of_deaths']
+result['deceased_characters'] = temp['characters']
+print(result)
+
+with open('info.json', 'w') as file:
+    json.dump(result, file, indent=4)
